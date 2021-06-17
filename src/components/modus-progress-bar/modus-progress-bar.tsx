15ad69ccf3e374a583feb941fb8af7cc9fa79faa
Ryan Henness <ryan_henness@trimble.com>
2021-06-17 11:41:56 -0700

    Move methods above render method.

--- src/components/modus-progress-bar/modus-progress-bar.tsx
+++ src/components/modus-progress-bar/modus-progress-bar.tsx
@@ -29,24 +29,6 @@ export class ModusProgressBar {
   /** (optional) The progress bar's value. */
   @Prop() value = 0;
 
-  render(): unknown {
-    const percentage = (this.value - this.minValue) / (this.maxValue - this.minValue) * 100;
-    const progressBarBackgroundColorClass = this.backgroundColor ? '' : 'default-background-color';
-    const progressColorClass = this.color ? '' : 'default-color';
-    const progressTextColor = this.textColor ? '' : 'default-text-color';
-
-    const progressBarClass = `modus-progress-bar ${progressBarBackgroundColorClass}`;
-    const progressClass = `progress ${progressColorClass} ${progressTextColor}`;
-
-    return (
-      <div class={progressBarClass} style={this.getProgressBarStyle()}>
-        <div class={progressClass} style={this.getProgressStyle(percentage)}>
-          {this.text}
-        </div>
-      </div>
-    );
-  }
-
   getProgressStyle(percentage: number): { backgroundColor: string, color: string, width: string } {
     const progressStyle = {
       backgroundColor: this.color,
@@ -63,5 +45,23 @@ export class ModusProgressBar {
     };
 
     return progressBarStyle;
+  }
+
+  render(): unknown {
+    const percentage = (this.value - this.minValue) / (this.maxValue - this.minValue) * 100;
+    const progressBarBackgroundColorClass = this.backgroundColor ? '' : 'default-background-color';
+    const progressColorClass = this.color ? '' : 'default-color';
+    const progressTextColor = this.textColor ? '' : 'default-text-color';
+
+    const progressBarClass = `modus-progress-bar ${progressBarBackgroundColorClass}`;
+    const progressClass = `progress ${progressColorClass} ${progressTextColor}`;
+
+    return (
+      <div class={progressBarClass} style={this.getProgressBarStyle()}>
+        <div class={progressClass} style={this.getProgressStyle(percentage)}>
+          {this.text}
+        </div>
+      </div>
+    );
   }
 }
