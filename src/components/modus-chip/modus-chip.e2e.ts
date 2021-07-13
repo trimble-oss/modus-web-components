475f93ac549fa0369062655474c148156054085c
Ryan Henness <ryan_henness@trimble.com>
2021-07-13 10:12:17 -0700

    Add chip component.

---
+++ src/components/modus-chip/modus-chip.e2e.ts
@@ -0,0 +1,162 @@
+import { newE2EPage } from '@stencil/core/testing';
+
+describe('modus-chip', () => {
+  it('renders', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<modus-chip></modus-chip>');
+    const element = await page.find('modus-chip');
+    expect(element).toHaveClass('hydrated');
+  });
+
+  it('renders changes to chip style', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<modus-chip></modus-chip>');
+    const chip = await page.find('modus-chip');
+    expect(await chip.getProperty('chipStyle')).toEqual('solid');
+
+    chip.setProperty('chipStyle', 'outline');
+    await page.waitForChanges();
+    expect(await chip.getProperty('chipStyle')).toEqual('outline');
+
+    const shadowContainer = await page.find('modus-chip >>> .modus-chip');
+    expect(await shadowContainer.classList.contains('style-outline'));
+  });
+
+  it('renders changes to disabled', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<modus-chip></modus-chip>');
+    const chip = await page.find('modus-chip');
+    expect(await chip.getProperty('disabled')).toBeFalsy();
+
+    chip.setProperty('disabled', 'true');
+    await page.waitForChanges();
+    expect(await chip.getProperty('disabled')).toBeTruthy();
+
+    const shadowContainer = await page.find('modus-chip >>> .modus-chip');
+    expect(await shadowContainer.classList.contains('disabled'));
+  });
+
+  it('renders changes to hasError', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<modus-chip></modus-chip>');
+    const chip = await page.find('modus-chip');
+    expect(await chip.getProperty('hasError')).toBeFalsy();
+
+    chip.setProperty('hasError', 'true');
+    await page.waitForChanges();
+    expect(await chip.getProperty('hasError')).toBeTruthy();
+
+    const shadowContainer = await page.find('modus-chip >>> .modus-chip');
+    expect(await shadowContainer.classList.contains('has-error'));
+  });
+
+  it('renders changes to imageUrl', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<modus-chip show-checkmark></modus-chip>');
+    const chip = await page.find('modus-chip');
+    expect(await chip.getProperty('imageUrl')).toBeFalsy();
+
+    chip.setProperty('imageUrl', 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png');
+    await page.waitForChanges();
+    expect(await chip.getProperty('imageUrl')).toBeTruthy();
+
+    const shadowImage = await page.find('modus-chip >>> img');
+    expect(shadowImage).not.toBeNull();
+
+    const shadowIconCheck = await page.find('modus-chip >>> .icon-check');
+    expect(shadowIconCheck).toBeNull();
+  });
+
+  it('renders changes to show checkmark', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<modus-chip></modus-chip>');
+    const chip = await page.find('modus-chip');
+    expect(await chip.getProperty('showCheckmark')).toBeFalsy();
+
+    chip.setProperty('showCheckmark', 'true');
+    await page.waitForChanges();
+    expect(await chip.getProperty('showCheckmark')).toBeTruthy();
+
+    const shadowIconCheck = await page.find('modus-chip >>> .icon-check');
+    expect(shadowIconCheck).not.toBeNull();
+  });
+
+  it('renders changes to show close', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<modus-chip></modus-chip>');
+    const chip = await page.find('modus-chip');
+    expect(await chip.getProperty('showClose')).toBeFalsy();
+
+    chip.setProperty('showClose', 'true');
+    await page.waitForChanges();
+    expect(await chip.getProperty('showClose')).toBeTruthy();
+
+    const shadowIconRemove = await page.find('modus-chip >>> .icon-remove');
+    expect(shadowIconRemove).not.toBeNull();
+  });
+
+  it('renders changes to size', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<modus-chip></modus-chip>');
+    const chip = await page.find('modus-chip');
+    expect(await chip.getProperty('size')).toEqual('medium');
+
+    chip.setProperty('size', 'large');
+    await page.waitForChanges();
+    expect(await chip.getProperty('size')).toEqual('large');
+
+    const shadowContainer = await page.find('modus-chip >>> .modus-chip');
+    expect(await shadowContainer.classList.contains('large'));
+  });
+
+  it('renders changes to value', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<modus-chip></modus-chip>');
+    const chip = await page.find('modus-chip');
+    expect(await chip.getProperty('value')).toBeFalsy();
+
+    chip.setProperty('value', 'Hello');
+    await page.waitForChanges();
+    expect(await chip.getProperty('value')).toEqual('Hello');
+
+    const shadowValue = await page.find('modus-chip >>> span');
+    expect(await shadowValue.textContent).toEqual('Hello');
+  });
+
+  it('emits chip click event on chip click', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<modus-chip></modus-chip>');
+    const chipClick = await page.spyOnEvent('chipClick');
+    const shadowContainer = await page.find('modus-chip >>> .modus-chip')
+    await page.waitForChanges();
+
+    await shadowContainer.click();
+    await page.waitForChanges();
+    expect(chipClick).toHaveReceivedEvent();
+  });
+
+  it('emits close click event on close icon click', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<modus-chip show-close></modus-chip>');
+    const closeClick = await page.spyOnEvent('closeClick');
+    const chipClick = await page.spyOnEvent('chipClick');
+    const shadowCloseIcon = await page.find('modus-chip >>> .icon-remove')
+    await page.waitForChanges();
+
+    await shadowCloseIcon.click();
+    await page.waitForChanges();
+    expect(chipClick).not.toHaveReceivedEvent();
+    expect(closeClick).toHaveReceivedEvent();
+  });
+});
---
+++ src/components/modus-chip/modus-chip.tsx
@@ -0,0 +1,85 @@
+// eslint-disable-next-line
+import { Component, Prop, h, EventEmitter, Event } from '@stencil/core';
+import { IconRemove } from '../icons/icon-remove';
+import { IconCheck } from '../icons/icon-check';
+
+@Component({
+  tag: 'modus-chip',
+  styleUrl: 'modus-chip.scss',
+  shadow: true,
+})
+
+export class ModusChip {
+  /** (optional) The chip's style. */
+  @Prop() chipStyle: 'outline' | 'solid'  = 'solid';
+
+  /** (optional) Whether the chip is disabled. */
+  @Prop() disabled = false;
+
+  /** (optional) Whether the chip has an error. */
+  @Prop() hasError = false;
+
+  /** (optional) The image's url */
+  @Prop() imageUrl: string;
+
+  /** (optional) Whether to show the checkmark. */
+  @Prop() showCheckmark = false;
+
+  /** (optional) Whether to show the close icon. */
+  @Prop() showClose = false;
+
+  /** (optional) The chip's size. */
+  @Prop() size: 'medium' | 'large' = 'medium';
+
+  /** (optional) The chip's value. */
+  @Prop() value: string;
+
+  /** An event that fires on chip click. */
+  @Event() chipClick: EventEmitter<MouseEvent>;
+
+  /** An event that fires on close icon click. */
+  @Event() closeClick: EventEmitter<MouseEvent>;
+
+  classByChipStyle: Map<string, string> = new Map([
+    ['outline', 'style-outline'],
+    ['solid', 'style-solid'],
+  ]);
+  classBySize: Map<string, string> = new Map([
+    ['medium', 'medium'],
+    ['large', 'large'],
+  ]);
+
+  onChipClick(event: MouseEvent): void {
+    if (event.defaultPrevented) { return; } // Don't emit chipClick if closeClick has emitted.
+
+    this.chipClick.emit(event);
+  }
+
+  onCloseClick(event: MouseEvent): void {
+    this.closeClick.emit(event);
+    event.preventDefault();
+  }
+
+  render(): unknown {
+    const chipClass = `
+      modus-chip ${this.disabled ? 'disabled' : ''}
+      ${this.hasError ? 'has-error' : ''}
+      ${this.classByChipStyle.get(this.chipStyle)}
+      ${this.classBySize.get(this.size)}
+      ${!this.showCheckmark && !this.imageUrl ? 'no-left-icon' : null}
+      ${!this.showClose ? 'no-right-icon' : null}
+    `;
+
+    return (
+      <div class={chipClass} onClick={!this.disabled ? (event) => this.onChipClick(event) : null}>
+        {
+          this.imageUrl ? <img src={this.imageUrl}/> :
+          this.showCheckmark ? <IconCheck size={'24'}></IconCheck> :
+          null
+        }
+        <span>{this.value}</span>
+        {this.showClose ? <IconRemove onClick={!this.disabled ? (event) => this.onCloseClick(event) : null} size={'24'}></IconRemove> : null}
+      </div>
+    );
+  }
+}
