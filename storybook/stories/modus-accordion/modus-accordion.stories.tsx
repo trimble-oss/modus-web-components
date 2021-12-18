5d3c0154d913543e6ab2f5d7edac2bf98900121a
Ryan Henness <ryan_henness@trimble.com>
2021-12-17 16:57:41 -0800

    Add stories for accordion, breadcrumb, card, and modal.

---
+++ storybook/stories/modus-accordion/modus-accordion.stories.tsx
@@ -0,0 +1,25 @@
+// @ts-ignore: JSX/MDX with Stencil
+import docs from './modus-accordion-storybook-docs.mdx';
+import { html } from 'lit-html';
+
+export default {
+  title: 'Components/Accordion',
+  parameters: {
+    docs: {
+      page: docs,
+    },
+    options: {
+      isToolshown: true
+    },
+    previewTabs: {
+      canvas: {
+        hidden: true
+      }
+    }
+  },
+};
+
+const Template = () => html``;
+export const Default = Template.bind({});
+
+
