4f1b0d530a792d23f148b6f6babd39e66b96596f
Christian Oliff <christianoliff@pm.me>
2022-01-11 20:57:52 +0900

    Add Prettier Config
    This just runs on .mdx files for now. We can potentially expand to other files in future.
    
    Prettier Config rules:
    - https://prettier.io/docs/en/options.html#print-width
    uses longer line lengths than the default (80). This means less changes in the codebase and makes things easier to read imo.
    - https://prettier.io/docs/en/options.html#bracket-line
    This is a good one which can make code sample appear nicer. see example in URL above to see.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,9 @@ module.exports = {
     'es2021': true
   },
   'extends': [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:storybook/recommended"
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:storybook/recommended'
   ],
   'parser': '@typescript-eslint/parser',
   'parserOptions': {
