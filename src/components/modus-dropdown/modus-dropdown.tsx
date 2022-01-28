fa43a9382f5a48b097be22a2a130a905175c356f
Ryan Henness <ryan_henness@trimble.com>
2022-01-28 09:32:38 -0800

    Fix modus-dropdown issue when used with shadow dom

--- src/components/modus-dropdown/modus-dropdown.tsx
+++ src/components/modus-dropdown/modus-dropdown.tsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line
-import { Component, Prop, h, Event, EventEmitter, Listen, State } from '@stencil/core';
+import { Component, Prop, h, Event, EventEmitter, State, Element } from '@stencil/core';
 
 @Component({
   tag: 'modus-dropdown',
@@ -7,26 +7,40 @@ import { Component, Prop, h, Event, EventEmitter, Listen, State } from '@stencil
   shadow: true,
 })
 export class ModusDropdown {
+  /** Reference to host HTML element. */
+  @Element() el: HTMLElement;
+
   /** (optional) The dropdown's aria-label. */
   @Prop() ariaLabel: string;
 
-  /** (optional) Disables the dropdown */
+  /** (optional) Disables the dropdown. */
   @Prop() disabled: boolean; // TODO
 
-  /** (optional) The placement of the dropdown in related to the toggleElement */
+  /** (optional) The placement of the dropdown in related to the toggleElement. */
   @Prop() placement: 'top' | 'right' | 'bottom' | 'left' = 'bottom';
 
-  /** (required) The element id that the list renders near and that triggers the toggling of the list */
+  /** (required) The element id that the list renders near and that triggers the toggling of the list. */
   @Prop() toggleElementId: string;
 
-  /** An event that fires on dropdown close */
+  /** An event that fires on dropdown close. */
   @Event() dropdownClose: EventEmitter;
 
-  @State() toggleElement: HTMLElement;
   @State() visible: boolean;
 
-  @Listen('click', { target: 'document' })
-  documentClickHandler(event: MouseEvent): void {
+  classByPlacement: Map<string, string> = new Map([
+    ['top', 'top'],
+    ['right', 'right'],
+    ['bottom', 'bottom'],
+    ['left', 'left'],
+  ]);
+  toggleElement: HTMLElement;
+
+  componentDidRender(): void {
+    this.toggleElement = this.el.querySelector(`#${this.toggleElementId}`);
+    if (!this.toggleElement) { throw Error('matching element not found for toggle-element-id'); }
+  }
+
+  handleDropdownClick(event: MouseEvent): void {
     if (!event.defaultPrevented) {
       if ((event.target as HTMLElement).closest(`#${this.toggleElementId}`)) {
         this.visible = !this.visible;
@@ -40,25 +54,13 @@ export class ModusDropdown {
     }
   }
 
-  classByPlacement: Map<string, string> = new Map([
-    ['top', 'top'],
-    ['right', 'right'],
-    ['bottom', 'bottom'],
-    ['left', 'left'],
-  ]);
-
-  componentWillLoad(): void {
-    this.toggleElement = document.querySelector(`#${this.toggleElementId}`);
-    if (!this.toggleElement) { throw Error('matching element not found for toggle-element-id'); }
-  }
-
   render(): unknown {
     const listContainerClass = `dropdown-list ${this.visible ? 'visible' : 'hidden'} ${this.classByPlacement.get(this.placement)}`;
-    const left = this.placement === 'right' ? `${this.toggleElement.offsetWidth}px` : 'unset';
-    const width = `${this.toggleElement.offsetWidth ? this.toggleElement.offsetWidth : 0}px`;
+    const left = this.placement === 'right' ? `${this.toggleElement?.offsetWidth}px` : 'unset';
+    const width = `${this.toggleElement?.offsetWidth ? this.toggleElement?.offsetWidth : 0}px`;
 
     return (
-      <div aria-label={this.ariaLabel} class="dropdown" role="listbox">
+      <div aria-label={this.ariaLabel} class="dropdown" role="listbox" onClick={event => this.handleDropdownClick(event)}>
         <slot name="dropdownToggle" />
         <div class={listContainerClass} style={{'left': left, 'min-width': width}}>
           <slot name="dropdownList" />
