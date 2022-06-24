e274dbe4089b5fecd9a0a8a1f88de6876a9ec2b3
Ryan Henness <ryan_henness@trimble.com>
2022-06-24 11:12:35 -0700

    Add React integration output

---
+++ react-workspace/src/components/stencil-generated/index.ts
@@ -0,0 +1,43 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@trimble-oss/modus-web-components';
+
+import { defineCustomElements } from '@trimble-oss/modus-web-components/loader';
+
+defineCustomElements();
+export const ModusAccordion = /*@__PURE__*/createReactComponent<JSX.ModusAccordion, HTMLModusAccordionElement>('modus-accordion');
+export const ModusAccordionItem = /*@__PURE__*/createReactComponent<JSX.ModusAccordionItem, HTMLModusAccordionItemElement>('modus-accordion-item');
+export const ModusAlert = /*@__PURE__*/createReactComponent<JSX.ModusAlert, HTMLModusAlertElement>('modus-alert');
+export const ModusBadge = /*@__PURE__*/createReactComponent<JSX.ModusBadge, HTMLModusBadgeElement>('modus-badge');
+export const ModusBreadcrumb = /*@__PURE__*/createReactComponent<JSX.ModusBreadcrumb, HTMLModusBreadcrumbElement>('modus-breadcrumb');
+export const ModusButton = /*@__PURE__*/createReactComponent<JSX.ModusButton, HTMLModusButtonElement>('modus-button');
+export const ModusCard = /*@__PURE__*/createReactComponent<JSX.ModusCard, HTMLModusCardElement>('modus-card');
+export const ModusCheckbox = /*@__PURE__*/createReactComponent<JSX.ModusCheckbox, HTMLModusCheckboxElement>('modus-checkbox');
+export const ModusChip = /*@__PURE__*/createReactComponent<JSX.ModusChip, HTMLModusChipElement>('modus-chip');
+export const ModusContentTreeItem = /*@__PURE__*/createReactComponent<JSX.ModusContentTreeItem, HTMLModusContentTreeItemElement>('modus-content-tree-item');
+export const ModusDropdown = /*@__PURE__*/createReactComponent<JSX.ModusDropdown, HTMLModusDropdownElement>('modus-dropdown');
+export const ModusFileDropzone = /*@__PURE__*/createReactComponent<JSX.ModusFileDropzone, HTMLModusFileDropzoneElement>('modus-file-dropzone');
+export const ModusList = /*@__PURE__*/createReactComponent<JSX.ModusList, HTMLModusListElement>('modus-list');
+export const ModusListItem = /*@__PURE__*/createReactComponent<JSX.ModusListItem, HTMLModusListItemElement>('modus-list-item');
+export const ModusMessage = /*@__PURE__*/createReactComponent<JSX.ModusMessage, HTMLModusMessageElement>('modus-message');
+export const ModusModal = /*@__PURE__*/createReactComponent<JSX.ModusModal, HTMLModusModalElement>('modus-modal');
+export const ModusNavbar = /*@__PURE__*/createReactComponent<JSX.ModusNavbar, HTMLModusNavbarElement>('modus-navbar');
+export const ModusNavbarAppsMenu = /*@__PURE__*/createReactComponent<JSX.ModusNavbarAppsMenu, HTMLModusNavbarAppsMenuElement>('modus-navbar-apps-menu');
+export const ModusNavbarMainMenu = /*@__PURE__*/createReactComponent<JSX.ModusNavbarMainMenu, HTMLModusNavbarMainMenuElement>('modus-navbar-main-menu');
+export const ModusNavbarNotificationsMenu = /*@__PURE__*/createReactComponent<JSX.ModusNavbarNotificationsMenu, HTMLModusNavbarNotificationsMenuElement>('modus-navbar-notifications-menu');
+export const ModusNavbarProfileMenu = /*@__PURE__*/createReactComponent<JSX.ModusNavbarProfileMenu, HTMLModusNavbarProfileMenuElement>('modus-navbar-profile-menu');
+export const ModusNumberInput = /*@__PURE__*/createReactComponent<JSX.ModusNumberInput, HTMLModusNumberInputElement>('modus-number-input');
+export const ModusPagination = /*@__PURE__*/createReactComponent<JSX.ModusPagination, HTMLModusPaginationElement>('modus-pagination');
+export const ModusProgressBar = /*@__PURE__*/createReactComponent<JSX.ModusProgressBar, HTMLModusProgressBarElement>('modus-progress-bar');
+export const ModusRadioGroup = /*@__PURE__*/createReactComponent<JSX.ModusRadioGroup, HTMLModusRadioGroupElement>('modus-radio-group');
+export const ModusSelect = /*@__PURE__*/createReactComponent<JSX.ModusSelect, HTMLModusSelectElement>('modus-select');
+export const ModusSlider = /*@__PURE__*/createReactComponent<JSX.ModusSlider, HTMLModusSliderElement>('modus-slider');
+export const ModusSpinner = /*@__PURE__*/createReactComponent<JSX.ModusSpinner, HTMLModusSpinnerElement>('modus-spinner');
+export const ModusSwitch = /*@__PURE__*/createReactComponent<JSX.ModusSwitch, HTMLModusSwitchElement>('modus-switch');
+export const ModusTabs = /*@__PURE__*/createReactComponent<JSX.ModusTabs, HTMLModusTabsElement>('modus-tabs');
+export const ModusTextInput = /*@__PURE__*/createReactComponent<JSX.ModusTextInput, HTMLModusTextInputElement>('modus-text-input');
+export const ModusToast = /*@__PURE__*/createReactComponent<JSX.ModusToast, HTMLModusToastElement>('modus-toast');
+export const ModusTooltip = /*@__PURE__*/createReactComponent<JSX.ModusTooltip, HTMLModusTooltipElement>('modus-tooltip');
