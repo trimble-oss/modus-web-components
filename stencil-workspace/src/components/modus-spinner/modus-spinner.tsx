534db1a26a4f93beb680888bdb67779a7f61fa14
Mahalakshmi Sankaran <mahalakshmi_sankaran@trimble.com>
2022-11-16 17:36:47 +0530

    Dark mode for Slider, Spinner, Tabs, Toasts, and Tooltips

--- stencil-workspace/src/components/modus-spinner/modus-spinner.tsx
+++ stencil-workspace/src/components/modus-spinner/modus-spinner.tsx
@@ -8,18 +8,23 @@ import { Component, Prop, h } from '@stencil/core';
 })
 export class ModusSpinner {
   /** (optional) The color of the spinner in hex format (eg #FFFFFF) */
-  @Prop() color = '#005F9E';
+  @Prop() color = 'primary';
 
   /** (optional) The size of the spinner, will be applied to both the height and width */
   @Prop() size = '2rem';
 
+  private variants: Map<string, string> = new Map([
+    ['primary', '#0063a3'],
+    ['secondary', '#6a6e79'],
+    ['tertiary', '#cbcdd6'],
+    ['dark', '#252a2e'],
+    ['success', '#006638'],
+    ['danger', '#da212c'],
+    ['warning', '#e49325'],
+  ]);
   render(): unknown {
-    return (
-      <div
-        aria-busy="true"
-        class="spinner"
-        style={{'border-color': this.color, 'border-right-color': 'transparent', 'height': this.size, 'width': this.size}}>
-      </div>
-    );
+    const borderColor = this.variants.has(this.color) ? `var(--modus-${this.color}, ${this.variants.get(this.color)})` : this.color;
+
+    return <div aria-busy="true" class="spinner" style={{ 'border-color': borderColor, 'border-right-color': 'transparent', height: this.size, width: this.size }}></div>;
   }
 }
