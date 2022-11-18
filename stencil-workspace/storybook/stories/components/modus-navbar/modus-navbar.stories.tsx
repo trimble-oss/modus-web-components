c783318cfcea1269e6f2be91282bd32faeb7d7de
Ryan Henness <84749026+ryan-henness-trimble@users.noreply.github.com>
2022-11-18 00:20:28 -0800

    Example for Navbar's Avatar initials fallback (#866)
    Co-authored-by: Christian Oliff <christianoliff@pm.me>
    Fixes https://github.com/trimble-oss/modus-web-components/issues/392

--- stencil-workspace/storybook/stories/components/modus-navbar/modus-navbar.stories.tsx
+++ stencil-workspace/storybook/stories/components/modus-navbar/modus-navbar.stories.tsx
@@ -6,7 +6,7 @@ export default {
   title: 'Components/Navbar',
   parameters: {
     docs: {
-      inlineStories: false,
+      inlineStories: true,
       page: docs,
     },
     options: {
@@ -20,23 +20,36 @@ export default {
 };
 
 const Template = () => html`
-  <modus-navbar show-apps-menu show-help-menu show-main-menu show-notifications>
+  <modus-navbar id="working" show-apps-menu show-help-menu show-main-menu show-notifications>
     <div slot="main">Render your own main menu.</div>
     <div slot="notifications">Render your own notifications.</div>
   </modus-navbar>
-  ${setNavbar()}
+  ${setNavbar(true, '#working')}
 `;
 export const Default = Template.bind({});
 
-const setNavbar = () => {
+const FailedToLoadAvatarTemplate = () => html`
+  <modus-navbar id="broken" show-apps-menu show-help-menu show-main-menu show-notifications>
+    <div slot="main">Render your own main menu.</div>
+    <div slot="notifications">Render your own notifications.</div>
+  </modus-navbar>
+  ${setNavbar(false, '#broken')}
+`;
+export const FailedAvatar = FailedToLoadAvatarTemplate.bind({});
+
+const setNavbar = (workingAvatar: boolean, id: string) => {
   const tag = document.createElement('script');
+  const avatarUrl = workingAvatar
+    ? 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0e738c17-7f3c-422e-8225-f8c782b08626/d9pordj-43d4aa59-54b0-46a1-a568-e36dd691cf27.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzBlNzM4YzE3LTdmM2MtNDIyZS04MjI1LWY4Yzc4MmIwODYyNlwvZDlwb3Jkai00M2Q0YWE1OS01NGIwLTQ2YTEtYTU2OC1lMzZkZDY5MWNmMjcucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.xvDk9KFIUAx0yAG3BPamDfRqmWUX6zwR4WVW40GjsoY'
+    : 'broken-link';
+
   tag.innerHTML = `
-        document.querySelector('modus-navbar').apps = [
+        document.querySelector('${id}').apps = [
           { description: 'The One Trimble Design System', logoUrl: 'https://modus.trimble.com/favicon.svg', name: 'Trimble Modus', url: 'https://modus.trimble.com/' }
         ];
-        document.querySelector('modus-navbar').productLogoOptions = { url: 'https://modus.trimble.com/img/trimble-logo.svg' };
-        document.querySelector('modus-navbar').profileMenuOptions = {
-          avatarUrl: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0e738c17-7f3c-422e-8225-f8c782b08626/d9pordj-43d4aa59-54b0-46a1-a568-e36dd691cf27.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzBlNzM4YzE3LTdmM2MtNDIyZS04MjI1LWY4Yzc4MmIwODYyNlwvZDlwb3Jkai00M2Q0YWE1OS01NGIwLTQ2YTEtYTU2OC1lMzZkZDY5MWNmMjcucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.xvDk9KFIUAx0yAG3BPamDfRqmWUX6zwR4WVW40GjsoY',
+        document.querySelector('${id}').productLogoOptions = { url: 'https://modus.trimble.com/img/trimble-logo.svg' };
+        document.querySelector('${id}').profileMenuOptions = {
+          avatarUrl: '${avatarUrl}',
           email: 'modus_user@trimble.com',
           initials: 'MU',
           username: 'Modus User',
