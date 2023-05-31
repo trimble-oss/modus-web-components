4db2efa79f1b9b27ea4a4f2bb85ba547414a4162
Ryan Henness <ryan_henness@trimble.com>
2023-05-31 16:48:07 -0700

    Upgrade guide template

--- stencil-workspace/storybook/.storybook/preview.js
+++ stencil-workspace/storybook/.storybook/preview.js
@@ -4,19 +4,16 @@ import yourTheme from './your-theme';
 import { themes } from '@storybook/theming';
 import addons from '@storybook/addons';
 import { DocsContainer } from '@storybook/addon-docs';
-import {
-  useDarkMode
-} from 'storybook-dark-mode';
-import '../../dist/modus-web-components/modus-web-components.css'
+import { useDarkMode } from 'storybook-dark-mode';
+import '../../dist/modus-web-components/modus-web-components.css';
 
 defineCustomElements();
 
 // get channel to listen to event emitter
 const channel = addons.getChannel();
 
-
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
@@ -28,10 +25,10 @@ export const parameters = {
     current: 'light',
     // Override the default dark theme
     dark: { ...themes.dark, appBg: '#252a2e' },
-    stylePreview: true
+    stylePreview: true,
   },
   backgrounds: {
-    disable: true
+    disable: true,
   },
   docs: {
     theme: yourTheme,
@@ -43,12 +40,12 @@ export const parameters = {
         ['Welcome', 'Getting Started'],
         'Components',
         'User Inputs',
-        'Framework Integrations'
-      ]
-    }
+        'Framework Integrations',
+        'Miscellaneous',
+      ],
+    },
   },
   previewTabs: {
     'storybook/docs/panel': { index: -1 },
-  }
-}
-
+  },
+};
