56a9f4ef232f08d621ef4398e54c0e3060f7bc02
sankaranarayanau <sankaranarayanau@nousinfo.com>
2023-06-13 11:17:41 +0530

    Modus checkbox focus state correction

--- stencil-workspace/src/components/modus-checkbox/modus-checkbox.spec.tsx
+++ stencil-workspace/src/components/modus-checkbox/modus-checkbox.spec.tsx
@@ -10,8 +10,8 @@ describe('modus-checkbox', () => {
     expect(page.root).toEqualHtml(`
       <modus-checkbox>
         <mock:shadow-root>
-          <div class="modus-checkbox" tabindex="0">
-            <div class="checkbox">
+          <div class="modus-checkbox">
+            <div class="checkbox" tabindex="0">
               <div class="checkmark">
                 <svg class="icon-check" fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
                   <path clip-rule="evenodd" d="M9.08471 15.4676L5.29164 11.736L4 12.9978L9.08471 18L20 7.26174L18.7175 6L9.08471 15.4676Z" fill="#FFFFFF" fill-rule="evenodd"></path>
