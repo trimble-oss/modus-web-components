620d69dae029270bc7e5a0ef38aac77740f7c798
Mahalakshmi Sankaran <mahalakshmi_sankaran@trimble.com>
2023-06-22 14:30:02 +0530

    Remove duplicate ModusNavbarProfileMenuLink export

--- stencil-workspace/src/interfaces.d.ts
+++ stencil-workspace/src/interfaces.d.ts
@@ -1,7 +1,6 @@
 export { Components, JSX } from './components';
 export { Crumb } from './components/modus-breadcrumb/modus-breadcrumb';
 export { ModusNavbarApp } from './components/modus-navbar/apps-menu/modus-navbar-apps-menu';
-export { ModusNavbarProfileMenuLink } from './components/modus-navbar/profile-menu/modus-navbar-profile-menu';
 export * from './components/modus-data-table/modus-data-table.models';
 export { ModusAutocompleteOption } from './components/modus-autocomplete/modus-autocomplete';
 export { ModusSideNavigationItemInfo } from './components/modus-side-navigation/modus-side-navigation.models';
@@ -11,6 +10,6 @@ export {
   ModusNavbarTooltip,
   ModusNavbarProfileMenuLink,
   ModusProfileMenuOptions,
-  ModusNavbarLogo, 
-  ModusNavbarLogoOptions
+  ModusNavbarLogo,
+  ModusNavbarLogoOptions,
 } from './components/modus-navbar/modus-navbar.models';
