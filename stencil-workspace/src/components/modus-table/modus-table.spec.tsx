1a0b2069dd0b0d585fe19514e8d0709e7ca6dfec
Hemantkumar <hemantk@nousinfo.com>
2023-06-30 12:19:02 +0530

    modus table unit test cases

--- stencil-workspace/src/components/modus-table/modus-table.spec.tsx
+++ stencil-workspace/src/components/modus-table/modus-table.spec.tsx
@@ -1,21 +1,23 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { ModusTable } from './modus-table';
 
-xdescribe('modus-table', () => {
+describe('modus-table', () => {
   it('renders', async () => {
-    const page = await newSpecPage({
+    const { root } = await newSpecPage({
       components: [ModusTable],
       html: '<modus-table></modus-table>',
     });
-    expect(page.root).toEqualHtml(`
+    expect(root).toEqualHtml(`
       <modus-table>
         <mock:shadow-root>
-          <table class="borderless cell-borderless">
+          <table class="false" style="width: 100%">
             <thead>
-              <tr></tr>
+              <tr>
+              </tr>
             </thead>
             <tbody></tbody>
           </table>
+          <slot name="customFooter"></slot>
         </mock:shadow-root>
       </modus-table>
     `);
