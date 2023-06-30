6b54387ee0f4486d97291e7d133651a5c5b81d4a
Hemantkumar <hemantk@nousinfo.com>
2023-06-30 12:19:02 +0530

    modus table unit test cases

--- stencil-workspace/src/components/modus-table/modus-table.e2e.ts
+++ stencil-workspace/src/components/modus-table/modus-table.e2e.ts
@@ -1,11 +1,399 @@
-import { newE2EPage } from '@stencil/core/testing';
+import { E2EPage, newE2EPage } from '@stencil/core/testing';
+
+const MockColumns = [
+  {
+    header: 'Mock Column One',
+    accessorKey: 'mockColumnOne',
+    id: 'mock-column-one',
+    dataType: 'text',
+    footer: 'Total',
+  },
+  {
+    header: 'Mock Column Two',
+    accessorKey: 'mockColumnTwo',
+    id: 'mock-column-two',
+    dataType: 'integer',
+    showTotal: true,
+  },
+];
+
+const MockData = [
+  {
+    mockColumnOne: 'Mock Data One 1',
+    mockColumnTwo: 330160,
+  },
+  {
+    mockColumnOne: 'Mock Data One 2',
+    mockColumnTwo: 900293,
+  },
+];
 
 describe('modus-table', () => {
-  it('renders', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<modus-table></modus-table>');
+  let page: E2EPage;
 
+  beforeEach(async () => {
+    page = await newE2EPage();
+    await page.setContent('<modus-table />');
+  });
+
+  it('Renders', async () => {
     const element = await page.find('modus-table');
     expect(element).toHaveClass('hydrated');
+  });
+
+  it('Renders changes to column prop', async () => {
+    const component = await page.find('modus-table');
+    let header = await page.findAll('modus-table >>> th');
+    expect(header.length).toBe(0);
+
+    component.setProperty('columns', MockColumns);
+    await page.waitForChanges();
+    header = await page.findAll('modus-table >>> th');
+    expect(header.length).toBeGreaterThan(0);
+    expect(header[0].textContent).toBe(MockColumns[0].header);
+  });
+
+  it('Renders changes to data prop', async () => {
+    const component = await page.find('modus-table');
+    let row = await page.findAll('modus-table >>> td');
+    expect(row.length).toBe(0);
+
+    component.setProperty('columns', MockColumns);
+    component.setProperty('data', MockData);
+
+    await page.waitForChanges();
+    row = await page.findAll('modus-table >>> td');
+    expect(row.length).toBeGreaterThan(0);
+    expect(row[0].textContent).toBe(MockData[0].mockColumnOne);
+  });
+
+  it('Renders with sort icon when sort is enabled', async () => {
+    page = await newE2EPage();
+
+    await page.setContent('<modus-table />');
+    const component = await page.find('modus-table');
+    component.setProperty('columns', MockColumns);
+    component.setProperty('data', MockData);
+    component.setProperty('sort', false);
+
+    await page.waitForChanges();
+    let sortContainer = await page.find('modus-table >>> .can-sort');
+    expect(sortContainer).toBeNull();
+
+    component.setProperty('sort', true);
+    await page.waitForChanges();
+    sortContainer = await page.find('modus-table >>> .can-sort');
+    expect(sortContainer).not.toBeNull();
+  });
+
+  it('Renders with sort icon when showSortIconOnHover is enabled', async () => {
+    page = await newE2EPage();
+
+    await page.setContent('<modus-table />');
+    const component = await page.find('modus-table');
+    component.setProperty('columns', MockColumns);
+    component.setProperty('data', MockData);
+    component.setProperty('sort', true);
+    component.setProperty('showSortIconOnHover', true);
+
+    await page.waitForChanges();
+    const header = await page.find('modus-table >>> th');
+
+    let sortContainer = await page.find('modus-table >>> .can-sort.hidden');
+    expect(sortContainer).toBeNull();
+
+    await header.hover();
+    await page.waitForChanges();
+
+    sortContainer = await page.find('modus-table >>> .hidden');
+    expect(sortContainer).not.toBeNull();
+  });
+
+  it('Should output sort event on sort icon click with sort enabled', async () => {
+    const component = await page.find('modus-table');
+    component.setProperty('columns', MockColumns);
+    component.setProperty('data', MockData);
+    component.setProperty('sort', true);
+    await page.waitForChanges();
+    const sortEvent = await page.spyOnEvent('sortChange');
+
+    const header = await page.find('modus-table >>> .sort-icons');
+    await header.click();
+    await header.click();
+
+    expect(sortEvent).toHaveReceivedEventTimes(2);
+  });
+
+  it('Renders with correct sort icon when sort is enabled', async () => {
+    page = await newE2EPage();
+    await page.setContent('<modus-table />');
+
+    const component = await page.find('modus-table');
+
+    component.setProperty('columns', MockColumns);
+    component.setProperty('data', MockData);
+    component.setProperty('sort', true);
+    await page.waitForChanges();
+
+    let iconSortAZ = await page.find('modus-table >>> svg');
+    let dataTestId = iconSortAZ['__attributeMap']['__items'].find((item) => item['_name'] === 'data-test-id');
+    let dataTestIdValue = dataTestId && dataTestId['_value'];
+
+    expect(dataTestIdValue).toBe('iconSortZA');
+
+    const header = await page.find('modus-table >>> .sort-icons');
+    await header.click();
+    await page.waitForChanges();
+    iconSortAZ = await page.find('modus-table >>> svg');
+    dataTestId = iconSortAZ['__attributeMap']['__items'].find((item) => item['_name'] === 'data-test-id');
+    dataTestIdValue = dataTestId && dataTestId['_value'];
+    expect(dataTestIdValue).toBe('iconSortAZ');
+  });
+
+  it('Should output sort data on sort icon click with sort enabled', async () => {
+    const component = await page.find('modus-table');
+    component.setProperty('columns', MockColumns);
+    component.setProperty('data', MockData);
+    component.setProperty('sort', true);
+    await page.waitForChanges();
+    const sortEvent = await page.spyOnEvent('sortChange');
+
+    const header = await page.find('modus-table >>> .sort-icons');
+    await header.click();
+
+    let tableData = await page.findAll('modus-table >>> td');
+    let firstItem = tableData[1].textContent;
+    let secondItem = tableData[3].textContent;
+    // Ascending sort
+    expect(firstItem).toEqual(MockData[0].mockColumnTwo.toString());
+    expect(secondItem).toEqual(MockData[1].mockColumnTwo.toString());
+
+    await header.click();
+    await page.waitForChanges();
+
+    // Descending sort
+    tableData = await page.findAll('modus-table >>> td');
+    firstItem = tableData[1].textContent;
+    secondItem = tableData[3].textContent;
+
+    expect(firstItem).toEqual(MockData[1].mockColumnTwo.toString());
+    expect(secondItem).toEqual(MockData[0].mockColumnTwo.toString());
+
+    expect(sortEvent).toHaveReceivedEventTimes(2);
+  });
+
+  it('Check sort icon tooltip text for ascending and descending ', async () => {
+    page = await newE2EPage();
+    await page.setContent('<modus-table />');
+
+    const component = await page.find('modus-table');
+
+    component.setProperty('columns', MockColumns);
+    component.setProperty('data', MockData);
+    component.setProperty('sort', false);
+    await page.waitForChanges();
+
+    let sortContainer = await page.find('modus-table >>> .can-sort');
+    expect(sortContainer).toBeNull();
+
+    component.setProperty('sort', true);
+    await page.waitForChanges();
+
+    sortContainer = await page.find('modus-table >>> th');
+    expect(sortContainer).not.toBeNull();
+
+    let tooltip = await sortContainer.find('modus-tooltip');
+    let tooltipText = await tooltip.getProperty('text');
+
+    expect(tooltip).not.toBeNull();
+    expect(tooltipText).toEqual('Sort Ascending');
+
+    let header = await page.find('modus-table >>> .sort-icons');
+    await header.click();
+    await page.waitForChanges();
+    tooltip = await sortContainer.find('modus-tooltip');
+    tooltipText = await tooltip.getProperty('text');
+    expect(tooltipText).toEqual('Sorted Ascending');
+
+    header = await page.find('modus-table >>> .sort-icons');
+    await header.click();
+    await page.waitForChanges();
+    tooltip = await sortContainer.find('modus-tooltip');
+    tooltipText = await tooltip.getProperty('text');
+    expect(tooltipText).toEqual('Sorted Descending');
+  });
+
+  it('Render pagination when pagination is enabled', async () => {
+    page = await newE2EPage();
+    await page.setContent('<modus-table />');
+
+    const component = await page.find('modus-table');
+
+    component.setProperty('columns', MockColumns);
+    component.setProperty('data', MockData);
+    component.setProperty('pagination', false);
+    await page.waitForChanges();
+    let paginationContainer = await page.find('modus-table >>> .pagination-container');
+    expect(paginationContainer).toBeNull();
+
+    component.setProperty('pagination', true);
+    await page.waitForChanges();
+
+    paginationContainer = await page.find('modus-table >>> .pagination-container');
+    expect(paginationContainer).not.toBeNull();
+  });
+
+  it('Display page view when pagination is enabled', async () => {
+    page = await newE2EPage();
+    await page.setContent('<modus-table />');
+
+    const component = await page.find('modus-table');
+
+    component.setProperty('columns', MockColumns);
+    component.setProperty('data', MockData);
+    component.setProperty('pagination', false);
+    await page.waitForChanges();
+    let paginationContainer = await page.find('modus-table >>> .items-per-page');
+    expect(paginationContainer).toBeNull();
+
+    component.setProperty('pagination', true);
+    await page.waitForChanges();
+
+    paginationContainer = await page.find('modus-table >>> .items-per-page');
+    expect(paginationContainer).not.toBeNull();
+    expect(paginationContainer.textContent).toBeTruthy();
+    expect(paginationContainer.textContent.startsWith('Page View')).toBeTruthy();
+  });
+
+  it('Display showing result data when pagination enabled', async () => {
+    page = await newE2EPage();
+    await page.setContent('<modus-table />');
+
+    const component = await page.find('modus-table');
+
+    component.setProperty('columns', MockColumns);
+    component.setProperty('data', MockData);
+    component.setProperty('pagination', false);
+    await page.waitForChanges();
+    let paginationContainer = await page.find('modus-table >>> .pagination-and-count');
+    expect(paginationContainer).toBeNull();
+
+    component.setProperty('pagination', true);
+    await page.waitForChanges();
+
+    paginationContainer = await page.find('modus-table >>> .pagination-and-count > .total-count');
+    expect(paginationContainer).not.toBeNull();
+    expect(paginationContainer.textContent).toBe('Showing result1-2 of 2');
+  });
+
+  it('Display items per page list when pageSizeList is enabled', async () => {
+    page = await newE2EPage();
+    await page.setContent('<modus-table />');
+    const component = await page.find('modus-table');
+    component.setProperty('columns', MockColumns);
+    component.setProperty('data', MockData);
+    await page.waitForChanges();
+
+    component.setProperty('pagination', true);
+    await page.waitForChanges();
+
+    let options = await page.findAll('modus-table >>> .items-per-page option');
+    let optionsData = await Promise.all(options.map((option) => option.textContent));
+    expect(optionsData).toContain('10');
+    expect(optionsData).toContain('20');
+    expect(optionsData).toContain('50');
+
+    component.setProperty('pageSizeList', [7, 10, 15]);
+    await page.waitForChanges();
+
+    options = await page.findAll('modus-table >>> .items-per-page option');
+    optionsData = await Promise.all(options.map((option) => option.textContent));
+    expect(optionsData).toContain('7');
+    expect(optionsData).toContain('10');
+    expect(optionsData).toContain('15');
+  });
+
+  it('Renders custom footer when summaryRow is true', async () => {
+    page = await newE2EPage();
+    await page.setContent('<modus-table />');
+
+    const component = await page.find('modus-table');
+
+    component.setProperty('columns', MockColumns);
+    component.setProperty('data', MockData);
+    component.setProperty('summaryRow', false);
+    await page.waitForChanges();
+    let footerContainer = await page.find('modus-table >>> summary-row');
+    expect(footerContainer).toBeNull();
+
+    component.setProperty('summaryRow', true);
+    await page.waitForChanges();
+
+    footerContainer = await page.find('modus-table >>> .summary-row');
+    expect(footerContainer).not.toBeNull();
+  });
+
+  it('Displays custom footer text for the footer', async () => {
+    page = await newE2EPage();
+    await page.setContent('<modus-table />');
+
+    const component = await page.find('modus-table');
+
+    component.setProperty('columns', MockColumns);
+    component.setProperty('data', MockData);
+    component.setProperty('summaryRow', true);
+    await page.waitForChanges();
+
+    const footerContainer = await page.find('modus-table >>> .summary-row');
+    expect(footerContainer).not.toBeNull();
+
+    const tableData = await page.findAll('modus-table >>> .summary-row > td');
+    const tableDataText = await Promise.all(tableData.map((td) => td.textContent));
+
+    expect(tableDataText[0]).toEqual('Total');
+  });
+
+  it('Calculates the sum of row values for the footer', async () => {
+    page = await newE2EPage();
+    await page.setContent('<modus-table />');
+    const component = await page.find('modus-table');
+
+    component.setProperty('columns', MockColumns);
+    component.setProperty('data', MockData);
+    component.setProperty('summaryRow', true);
+    await page.waitForChanges();
+
+    const footerContainer = await page.find('modus-table >>> .summary-row');
+    expect(footerContainer).not.toBeNull();
+
+    const tableData = await page.findAll('modus-table >>> .summary-row > td');
+    const tableDataTexts = await Promise.all(tableData.map((td) => td.textContent));
+
+    expect(parseInt(tableDataTexts[1])).toEqual(MockData[0].mockColumnTwo + MockData[1].mockColumnTwo);
+  });
+
+  it('Formats the row values with currency format', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<modus-table />');
+
+    const component = await page.find('modus-table');
+    component.setProperty('columns', MockColumns);
+    component.setProperty('data', MockData);
+    await page.waitForChanges();
+
+    function formatCurrency(value) {
+      return '$' + formatNumber(value);
+    }
+
+    function formatNumber(value) {
+      return Number(value)
+        .toFixed(2)
+        .replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    }
+    const tableData = await page.findAll('modus-table >>> td');
+    const tableDataTexts = await Promise.all(tableData.map((td) => formatCurrency(td.textContent)));
+
+    expect(tableDataTexts[1]).toEqual('$330,160.00');
+    expect(tableDataTexts[3]).toEqual('$900,293.00');
   });
 });
