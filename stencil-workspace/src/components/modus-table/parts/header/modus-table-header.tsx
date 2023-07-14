de94418417f38904278dd68d79c39724e53471d6
Ankur Koul <ankurk@assurant.com>
2023-07-14 16:56:23 +0530

    Added functionality for Column Reorder

--- stencil-workspace/src/components/modus-table/parts/header/modus-table-header.tsx
+++ stencil-workspace/src/components/modus-table/parts/header/modus-table-header.tsx
@@ -5,32 +5,64 @@ import {
 import { Header, Table } from '@tanstack/table-core';
 import { ModusTableHeaderSort } from './modus-table-header-sort';
 import { ModusTableColumnResizingHandler } from './modus-table-column-resizing-handler';
+import { IconArrowDown } from '../../../icons/icon-arrow-down';
+import { IconArrowUp } from '../../../icons/icon-arrow-up';
+import { EnterKey } from '../../constants/constants';
 
 interface ModusTableHeaderProps {
   table: Table<unknown>;
   header: Header<unknown, unknown>;
   isNestedParentHeader: boolean;
   showSortIconOnHover: boolean;
+  columnReorder: boolean;
+  columnResizeEnabled: boolean;
+  handleDragStart: (event: MouseEvent, id: string, elementRef: HTMLTableHeaderCellElement) => void;
+  handleKeyboardStart: (event: KeyboardEvent, id: string, elementRef: HTMLTableHeaderCellElement) => void;
+  onMouseEnterResize: () => void;
+  onMouseLeaveResize: () => void;
 }
 
 /**
  * Modus Table Header
  */
-export const ModusTableHeader: FunctionalComponent<
-  ModusTableHeaderProps
-> = ({ table, header, isNestedParentHeader, showSortIconOnHover }) => {
+export const ModusTableHeader: FunctionalComponent<ModusTableHeaderProps> = ({
+  table,
+  header,
+  isNestedParentHeader,
+  showSortIconOnHover,
+  columnReorder,
+  columnResizeEnabled,
+  handleDragStart,
+  handleKeyboardStart,
+  onMouseEnterResize,
+  onMouseLeaveResize,
+}) => {
+  let elementRef: HTMLTableHeaderCellElement;
+  console.log(columnResizeEnabled);
+
   return (
     <th
+      tabindex={`${!columnResizeEnabled && columnReorder ? '0' : ''}`}
       key={header.id}
       colSpan={header.colSpan}
       class={`
         ${isNestedParentHeader ? 'text-align-center' : ''}
+        ${!columnResizeEnabled && columnReorder ? 'can-reorder' : ''}
+        ${columnResizeEnabled ? 'show-resize-cursor' : ''}
         ${header.column.getIsResizing() ? 'active-resize' : ''}
       `}
       style={{ width: `${header.getSize()}px` }}
       aria-label={header.column.columnDef.header}
       role="columnheader"
-      scope="col">
+      scope="col"
+      id={header.id}
+      ref={(element: HTMLTableHeaderCellElement) => (elementRef = element)}
+      onMouseDown={(event: MouseEvent) => handleDragStart(event, header.id, elementRef)}
+      onKeyDown={(event: KeyboardEvent) => {
+        if (event.key.toLowerCase() === EnterKey) {
+          handleKeyboardStart(event, header.id, elementRef);
+        }
+      }}>
       {header.isPlaceholder ? null : ( // header.isPlaceholder is Required for nested column headers to display empty cell
         <div class={header.column.getCanSort() && 'can-sort'}>
           <span>{header.column.columnDef.header}</span>
@@ -38,11 +70,21 @@ export const ModusTableHeader: FunctionalComponent<
             <ModusTableHeaderSort
               column={header.column}
               showSortIconOnHover={showSortIconOnHover}
+              columnResizeEnabled={columnResizeEnabled}
             />
           )}
         </div>
       )}
-      <ModusTableColumnResizingHandler table={table} header={header} />
+      {/** Column resizing handler */}
+      <ModusTableColumnResizingHandler
+        table={table}
+        header={header}
+        onMouseEnter={() => onMouseEnterResize()}
+        onMouseLeave={() => onMouseLeaveResize()}
+      />
+      {/** Icons for column reorder  */}
+      <IconArrowDown size={'16'} />
+      <IconArrowUp size={'16'} />
     </th>
   );
 };
