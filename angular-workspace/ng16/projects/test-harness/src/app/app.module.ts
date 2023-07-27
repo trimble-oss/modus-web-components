c6d8e2dff80286465d5ddce0086b9e8e63fc34c6
Christian Oliff <christianoliff@pm.me>
2023-07-28 00:15:02 +0900

    Add Angular 16 support
    Adds ng16 project

---
+++ angular-workspace/ng16/projects/test-harness/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import {
+  ModusAngularComponentsModule
+} from "../../../trimble-oss/modus-angular-components/src/lib/modus-angular-components.module";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ModusAngularComponentsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
