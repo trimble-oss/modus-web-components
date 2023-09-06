0319eda21530bb85d95662652b0818f7a2f03351
RoliverJavier <roliver.rodriguez@mindbodyonline.com>
2023-09-06 01:46:26 -0400

    Merge branch 'main' into feature/NAV-227

--- angular-workspace/projects/trimble-oss/modus-angular-components/src/lib/stencil-generated/index.ts
+++ angular-workspace/projects/trimble-oss/modus-angular-components/src/lib/stencil-generated/index.ts
@@ -1,3 +1,4 @@
+
 import * as d from './components';
 
 export const DIRECTIVES = [
@@ -44,5 +45,5 @@ export const DIRECTIVES = [
   d.ModusToast,
   d.ModusTooltip,
   d.ModusTreeView,
-  d.ModusTreeViewItem,
+  d.ModusTreeViewItem
 ];
