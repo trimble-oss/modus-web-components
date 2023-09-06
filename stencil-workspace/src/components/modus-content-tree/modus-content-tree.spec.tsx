6ae429a1b3cd574df8129cd8a0478abb7f048d97
RoliverJavier <roliver.rodriguez@mindbodyonline.com>
2023-09-06 02:29:06 -0400

    fix: changing some svg expected result value from test files

--- stencil-workspace/src/components/modus-content-tree/modus-content-tree.spec.tsx
+++ stencil-workspace/src/components/modus-content-tree/modus-content-tree.spec.tsx
@@ -37,20 +37,20 @@ describe('modus-tree-view-item', () => {
           <div class="icon-slot drag-icon hidden">
              <slot name="dragIcon"></slot>
              <svg class="icon-drag" height="16" width="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M11,18a2,2,0,1,1-2-2A2.006,2.006,0,0,1,11,18ZM9,10a2,2,0,1,0,2,2A2.006,2.006,0,0,0,9,10ZM9,4a2,2,0,1,0,2,2A2.006,2.006,0,0,0,9,4Zm6,4a2,2,0,1,0-2-2A2.006,2.006,0,0,0,15,8Zm0,2a2,2,0,1,0,2,2A2.006,2.006,0,0,0,15,10Zm0,6a2,2,0,1,0,2,2A2.006,2.006,0,0,0,15,16Z" fill="#252a2e"></path>
+                <path d="M11,18a2,2,0,1,1-2-2A2.006,2.006,0,0,1,11,18ZM9,10a2,2,0,1,0,2,2A2.006,2.006,0,0,0,9,10ZM9,4a2,2,0,1,0,2,2A2.006,2.006,0,0,0,9,4Zm6,4a2,2,0,1,0-2-2A2.006,2.006,0,0,0,15,8Zm0,2a2,2,0,1,0,2,2A2.006,2.006,0,0,0,15,10Zm0,6a2,2,0,1,0,2,2A2.006,2.006,0,0,0,15,16Z" fill="#6A6976"></path>
              </svg>
           </div>
           <div aria-disabled="true" style="padding-left: 0rem;"></div>
           <div class="icon-slot hidden" tabindex="-1">
              <div class="inline-flex rotate-right">
                 <slot name="expandIcon"></slot>
-                <svg class="icon-chevron-down-thick" xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="24" width="24" viewBox="0 0 24 24">
+                <svg class="icon-chevron-down-thick" xmlns="http://www.w3.org/2000/svg" fill="#6A6976" height="24" width="24" viewBox="0 0 24 24">
                    <path d="M15.88 9.29 12 13.17 8.12 9.29a.996.996 0 1 0-1.41 1.41l4.59 4.59c.39.39 1.02.39 1.41 0l4.59-4.59a.996.996 0 0 0 0-1.41c-.39-.38-1.03-.39-1.42 0z"></path>
                 </svg>
              </div>
              <div class="inline-flex">
                 <slot name="collapseIcon"></slot>
-                <svg class="icon-chevron-down-thick" xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="24" width="24" viewBox="0 0 24 24">
+                <svg class="icon-chevron-down-thick" xmlns="http://www.w3.org/2000/svg" fill="#6A6976" height="24" width="24" viewBox="0 0 24 24">
                    <path d="M15.88 9.29 12 13.17 8.12 9.29a.996.996 0 1 0-1.41 1.41l4.59 4.59c.39.39 1.02.39 1.41 0l4.59-4.59a.996.996 0 0 0 0-1.41c-.39-.38-1.03-.39-1.42 0z"></path>
                 </svg>
              </div>
