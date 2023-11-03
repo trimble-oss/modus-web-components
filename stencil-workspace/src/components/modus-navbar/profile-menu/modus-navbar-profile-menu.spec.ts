3d78c8d74ad8074c49c748aa7a39ab1f815dd2b2
Alyssa Comstock <107583407+acomsto@users.noreply.github.com>
2023-11-03 09:22:20 -0700

    Add optional icons to profile menu link items in the navbar (#1776)

---
+++ stencil-workspace/src/components/modus-navbar/profile-menu/modus-navbar-profile-menu.spec.ts
@@ -0,0 +1,59 @@
+import { newSpecPage } from '@stencil/core/testing';
+import { ModusNavbarProfileMenu } from './modus-navbar-profile-menu';
+
+describe('modus-navbar-profile-menu', () => {
+  it('renders', async () => {
+    const { root } = await newSpecPage({
+      components: [ModusNavbarProfileMenu],
+      html: '<modus-navbar-profile-menu></modus-navbar-profile-menu>',
+    });
+
+    const profileMenuElement: HTMLModusNavbarProfileMenuElement = root.shadowRoot.querySelector('div.profile-menu');
+
+    expect(profileMenuElement).toBeTruthy();
+  });
+
+  it('renders with username', async () => {
+    const modusProfileMenu = new ModusNavbarProfileMenu();
+    modusProfileMenu.username = 'John Doe';
+
+    expect(modusProfileMenu.username).toEqual('John Doe');
+  });
+
+  it('renders with a profile avatar', async () => {
+    const modusProfileMenu = new ModusNavbarProfileMenu();
+    modusProfileMenu.avatarUrl = 'https://www.placeholder.com/150';
+
+    expect(modusProfileMenu.avatarUrl).toEqual('https://www.placeholder.com/150');
+  });
+
+  it('renders with a profile email', async () => {
+    const modusProfileMenu = new ModusNavbarProfileMenu();
+    modusProfileMenu.email = 'example@example.com';
+
+    expect(modusProfileMenu.email).toEqual('example@example.com');
+  });
+
+  it('renders with a profile initials', async () => {
+    const modusProfileMenu = new ModusNavbarProfileMenu();
+    modusProfileMenu.initials = 'AB';
+
+    expect(modusProfileMenu.initials).toEqual('AB');
+  });
+
+  it('renders with profile links', async () => {
+    const modusProfileMenu = new ModusNavbarProfileMenu();
+    modusProfileMenu.links = [{ id: 'link1', display: 'Link 1' }];
+
+    expect(modusProfileMenu.links).toEqual([{ id: 'link1', display: 'Link 1' }]);
+    expect(modusProfileMenu.links[0]).not.toHaveProperty('icon');
+  });
+
+  it('renders with profile links with icons', async () => {
+    const modusProfileMenu = new ModusNavbarProfileMenu();
+    modusProfileMenu.links = [{ id: 'link1', display: 'Link 1', icon: 'moon' }];
+
+    expect(modusProfileMenu.links).toEqual([{ id: 'link1', display: 'Link 1', icon: 'moon' }]);
+    expect(modusProfileMenu.links[0]).toHaveProperty('icon');
+  });
+});
