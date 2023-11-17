7c25a0860ab0f4e61e873bff9ed59646caf02761
Elisha Sam Peter Prabhu <Elisha_SamPeterPrabhu@Trimble.com>
2023-11-17 15:35:46 +0530

    Update Test Case

--- stencil-workspace/src/components/modus-checkbox/modus-checkbox.spec.tsx
+++ stencil-workspace/src/components/modus-checkbox/modus-checkbox.spec.tsx
@@ -13,7 +13,7 @@ describe('modus-checkbox', () => {
           <div class="modus-checkbox">
             <div class="checkbox" tabindex="0">
               <div class="checkmark">
-                <svg class="icon-check" fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                <svg class="icon-check" fill="none" height="16" viewBox="0 0 24 24" width="16" xmlns="http://www.w3.org/2000/svg">
                   <path clip-rule="evenodd" d="M9.08471 15.4676L5.29164 11.736L4 12.9978L9.08471 18L20 7.26174L18.7175 6L9.08471 15.4676Z" fill="#FFFFFF" fill-rule="evenodd"></path>
                 </svg>
               </div>
