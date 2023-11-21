17d281d719933478af43dd46631d9344f94d9a9c
Elisha Sam Peter Prabhu <Elisha_SamPeterPrabhu@Trimble.com>
2023-11-21 14:43:51 +0530

    Update css and storybook

--- stencil-workspace/storybook/stories/components/modus-radio-group/modus-radio-group.stories.tsx
+++ stencil-workspace/storybook/stories/components/modus-radio-group/modus-radio-group.stories.tsx
@@ -23,7 +23,12 @@ const Template = () => html`
   <modus-radio-group checked-id="1" name="my-group"></modus-radio-group>
   ${setRadioGroup()}
 `;
+const smallTemplate = () => html`
+  <modus-radio-group checked-id="1" name="my-group-small"></modus-radio-group>
+  ${setSmallRadioGroup()}
+`;
 export const Default = Template.bind({});
+export const Small = smallTemplate.bind({});
 
 const setRadioGroup = () => {
   const tag = document.createElement('script');
@@ -37,11 +42,36 @@ const setRadioGroup = () => {
         checked: true,
         id: '1',
         label: 'Radio 2',
-        size:'small'
       },
       {
         id: '2',
         label: 'Radio 3'
+      }
+    ];
+  `;
+
+  return tag;
+};
+
+const setSmallRadioGroup = () => {
+  const tag = document.createElement('script');
+  tag.innerHTML = `
+    document.querySelector('modus-radio-group').radioButtons = [
+      {
+        id: '0',
+        label: 'Radio 1',
+        size:'small'
+      },
+      {
+        checked: true,
+        id: '1',
+        label: 'Radio 2',
+        size:'small'
+      },
+      {
+        id: '2',
+        label: 'Radio 3',
+        size:'small'
       }
     ];
   `;
