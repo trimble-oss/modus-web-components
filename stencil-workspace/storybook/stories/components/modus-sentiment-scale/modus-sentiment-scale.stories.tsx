4fd6a1666b6695ec7f2756f5dae639e5d917d892
Elisha Sam Peter Prabhu <Elisha_SamPeterPrabhu@Trimble.com>
2023-11-21 13:20:16 +0530

    Update StoryBook

---
+++ stencil-workspace/storybook/stories/components/modus-sentiment-scale/modus-sentiment-scale.stories.tsx
@@ -0,0 +1,58 @@
+// @ts-ignore: JSX/MDX with Stencil
+import docs from './modus-sentiment-scale-storybook-docs.mdx';
+import { html } from 'lit-html';
+
+export default {
+  title: 'Components/Sentiment Scale',
+  argTypes: {
+    iconsType: {
+      name: 'icons-type',
+      control: {
+        options: ['smileys', 'thumbs'],
+        type: 'select',
+      },
+   },
+ disabled: {
+   description: 'Whether the chip is disabled',
+   table: {
+     defaultValue: { summary: false },
+     type: { summary: 'boolean' },
+   },
+  },
+},
+  parameters: {
+    docs: {
+      page: docs,
+    },
+    actions: {
+      handles: ['sentimentSelection'],
+    },
+    controls: {
+      expanded: true,
+    },
+    options: {
+      isToolshown: true,
+    },
+  },
+};
+
+const Template = ({iconsType,
+disabled}) => html`
+<modus-sentiment-scale icons-type="${iconsType}" disabled="${disabled}"></modus-sentiment-scale>
+
+<script>
+  const sentimentScale = document.querySelector('modus-sentiment-scale');
+  sentimentScale.addEventListener('sentimentSelection', (event) => {
+    console.log('Sentiment selected:', event.detail);
+  });
+</script>
+`;
+export const Default = Template.bind({});
+Default.args = {
+  disabled: false,
+  iconsType: 'smileys',
+};
+
+
+
+
