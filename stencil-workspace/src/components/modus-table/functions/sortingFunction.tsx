1fcd876ee28049a951389056724ad46299dd8e88
RoliverJavier <roliverjavier@gmail.com>
2023-12-08 12:16:36 -0400

    refactor util sorting function

--- stencil-workspace/src/components/modus-table/functions/sortingFunction.tsx
+++ stencil-workspace/src/components/modus-table/functions/sortingFunction.tsx
@@ -1,8 +1,16 @@
 import { Row } from '@tanstack/table-core';
 
-export function sortHyperlink(rowA: Row<unknown>, rowB: Row<unknown>, columnId: string): number {
-  const valA = rowA.getValue(columnId)?.['display'] ?? rowA.getValue(columnId);
-  const valB = rowB.getValue(columnId)?.['display'] ?? rowB.getValue(columnId);
+export function sortHyperlink(rowA: Row<unknown>, rowB: Row<unknown>, columnId: string){
+  return sortByValue(rowA, rowB, columnId, 'display');
+}
+
+export function sortBadge(rowA: Row<unknown>, rowB: Row<unknown>, columnId: string) {
+  return sortByValue(rowA, rowB, columnId, 'text');
+}
+
+function sortByValue(rowA: Row<unknown>, rowB: Row<unknown>, columnId: string, valueKey: string): number {
+  const valA = rowA.getValue(columnId)?.[valueKey] ?? rowA.getValue(columnId);
+  const valB = rowB.getValue(columnId)?.[valueKey] ?? rowB.getValue(columnId);
 
   // If valA is null, undefined or empty
   if (!valA) {
@@ -17,19 +25,3 @@ export function sortHyperlink(rowA: Row<unknown>, rowB: Row<unknown>, columnId: 
   return valA < valB ? -1 : 1;
 }
 
-export function sortBadge(rowA: Row<unknown>, rowB: Row<unknown>, columnId: string): number {
-  const valA = rowA.getValue(columnId)?.['text'] ?? rowA.getValue(columnId);
-  const valB = rowB.getValue(columnId)?.['text'] ?? rowB.getValue(columnId);
-
-  // If valA is null, undefined or empty
-  if (!valA) {
-    return 1;
-  }
-
-  // If valB is null, undefined or empty
-  if (!valB) {
-    return -1;
-  }
-
-  return valA < valB ? -1 : 1;
-}
