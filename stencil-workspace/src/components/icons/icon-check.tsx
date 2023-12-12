aec22dd1b95715b212e61f4ad91b38abaf95a583
coliff <christianoliff@pm.me>
2023-12-12 23:14:14 +0900

    fix tests

--- stencil-workspace/src/components/icons/icon-check.tsx
+++ stencil-workspace/src/components/icons/icon-check.tsx
@@ -14,12 +14,13 @@ export const IconCheck: FunctionalComponent<IconProps> = (props: IconProps) => (
     width={props.size ?? 16}
     onClick={props.onClick ? () => props.onClick() : null}
     viewBox="0 0 24 24"
-    fill={props.color ?? 'currentColor'}
+    fill="none"
     xmlns="http://www.w3.org/2000/svg">
     <path
       fill-rule="evenodd"
       clip-rule="evenodd"
       d="M9.08471 15.4676L5.29164 11.736L4 12.9978L9.08471 18L20 7.26174L18.7175 6L9.08471 15.4676Z"
+      fill={props.color ?? '#6A6976'}
     />
   </svg>
 );
