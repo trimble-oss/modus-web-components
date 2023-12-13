33296529bca760568962969236773805312ce1d8
Christian Oliff <christianoliff@pm.me>
2023-12-13 15:50:32 +0900

    Fix Icon Inconsistent colors and class names (#1946)
    * Fix Icon Inconsistent colors and class names
    
    Partial fix for #1889
    
    * fix tests
    
    * Update icon-indeterminate.tsx

--- stencil-workspace/src/components/icons/icon-arrow-down.tsx
+++ stencil-workspace/src/components/icons/icon-arrow-down.tsx
@@ -12,13 +12,12 @@ interface IconProps {
 export const IconArrowDown: FunctionalComponent<IconProps> = (props: IconProps) => (
   <svg
     class="icon-arrow-down"
-    xmlns="http://www.w3.org/2000/svg"
     fill={props.color ?? 'currentColor'}
     height={props.size ?? 16}
-    width={props.size ?? 16}
     onClick={props.onClick}
-    data-test-id="iconArrowDown"
-    viewBox="0 0 24 24">
+    viewBox="0 0 24 24"
+    width={props.size ?? 16}
+    xmlns="http://www.w3.org/2000/svg">
     <path d="M15 12h3.79c.45 0 .67.54.35.85l-6.44 6.44a.996.996 0 0 1-1.41 0l-6.44-6.44A.5.5 0 0 1 5.2 12h3.79V5c0-.55.45-1 1-1h4c.55 0 1 .45 1 1v7Z" />
   </svg>
 );
--- stencil-workspace/src/components/icons/icon-moon.tsx
+++ stencil-workspace/src/components/icons/icon-moon.tsx
@@ -5,15 +5,12 @@ import { IconProps } from './IconMap';
 export const IconMoon: FunctionalComponent<IconProps> = (props: IconProps) => (
   <svg
     class="icon-moon"
+    fill={props.color ?? 'currentColor'}
     height={props.size ?? 16}
-    width={props.size ?? 16}
     onClick={props.onClick}
     viewBox="0 0 24 24"
-    fill="none"
+    width={props.size ?? 16}
     xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M18.84 15.35c.44-.13.83.34.58.71-1.88 2.68-5.5 4.18-9.34 3.07-2.43-.7-4.39-2.53-5.16-4.82C3.44 9.91 6.03 5.8 10 4.52c.36-.12.67.27.46.58-1.09 1.68-1.45 3.83-.64 6.06.67 1.85 2.22 3.34 4.15 4.02 1.72.6 3.39.6 4.87.17Z"
-      fill={props.color ?? 'currentColor'}
-    />
+    <path d="M18.84 15.35c.44-.13.83.34.58.71-1.88 2.68-5.5 4.18-9.34 3.07-2.43-.7-4.39-2.53-5.16-4.82C3.44 9.91 6.03 5.8 10 4.52c.36-.12.67.27.46.58-1.09 1.68-1.45 3.83-.64 6.06.67 1.85 2.22 3.34 4.15 4.02 1.72.6 3.39.6 4.87.17Z" />
   </svg>
 );
