01d3f66a0a415cb947a2b702b9912e9e693c3183
Christian Oliff <christianoliff@pm.me>
2023-12-21 00:58:21 +0900

    Docs: Remove problematic id="card-title" and id="card-subtitle" from card examples (#1970)
    Setting `id="card-title"` and `id="card-subtitle"`  on all cards would result in invalid HTML (id's must be unique). These IDs serve no purpose and make the examples appear noisy.

--- angular-workspace/test-ng14/src/examples/modus-card-examples/modus-card-examples.component.ts
+++ angular-workspace/test-ng14/src/examples/modus-card-examples/modus-card-examples.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
     <modus-card>
       <div style="padding:10px">
         <h4 id="card-title">Card title</h4>
-        <h5 id="card-subtitle">Card subtitle</h5>
+        <h5>Card subtitle</h5>
         <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         <modus-button color="primary">Go somewhere</modus-button>
       </div>
--- stencil-workspace/storybook/stories/components/modus-card/modus-card.stories.tsx
+++ stencil-workspace/storybook/stories/components/modus-card/modus-card.stories.tsx
@@ -66,8 +66,8 @@ const Template = ({
   <modus-card aria-label=${ariaLabel} height=${height} width=${width} border-radius=${borderRadius} show-card-border=${showCardBorder} show-shadow-on-hover=${showShadowOnHover}>
     <!-- Render anything here -->
     <div style="padding:10px">
-      <h4 id="card-title">Card title</h4>
-      <h5 id="card-subtitle">Card subtitle</h5>
+      <h4>Card title</h4>
+      <h5>Card subtitle</h5>
       <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       <modus-button color="primary">Go somewhere</modus-button>
     </div>
