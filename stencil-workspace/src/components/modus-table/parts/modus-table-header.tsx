68e7eba7f30413928698eefb5fa2dd6eb2932af8
Venkatesh N <venkatesh_n@trimble.com>
2024-01-11 14:48:17 +0530

    Commented out unused variable

--- stencil-workspace/src/components/modus-table/parts/modus-table-header.tsx
+++ stencil-workspace/src/components/modus-table/parts/modus-table-header.tsx
@@ -35,7 +35,7 @@ export const ModusTableHeader: FunctionalComponent<ModusTableHeaderProps> = ({
 
   const tableHeadClass = { 'show-resize-cursor': getColumnResizing(), 'show-column-reorder-cursor': columnReorder };
   const headerGroups: HeaderGroup<unknown>[] = getHeaderGroups();
-  const rowActionsLength = Math.min(Math.max(rowActions.length * 40, 90), 160);
+  // const rowActionsLength = Math.min(Math.max(rowActions.length * 40, 90), 160);
 
   return (
     <thead class={tableHeadClass}>
