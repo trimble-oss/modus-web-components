93f242c252327f7f04f9059a243cfcb5cbdf0d53
Elisha Sam Peter Prabhu <Elisha_SamPeterPrabhu@Trimble.com>
2024-01-22 15:16:52 +0530

    Update activePage default value

--- stencil-workspace/src/components/modus-pagination/modus-pagination.tsx
+++ stencil-workspace/src/components/modus-pagination/modus-pagination.tsx
@@ -22,7 +22,7 @@ export class ModusPagination {
   @Prop() ariaLabel: string | null;
 
   /* The active page. */
-  @Prop({ mutable: true }) activePage: number;
+  @Prop({ mutable: true }) activePage = 1;
   @Watch('activePage')
   activePageWatch(newValue: number, oldValue: number) {
     if (newValue !== oldValue) {
