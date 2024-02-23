a2ecdb9dc79ce14a4e2e631a83d16f829b17c38a
Christopher Winsor <cjwinsor@users.noreply.github.com>
2024-02-23 09:54:44 -0500

    Update storybook for breadcrumbs

--- angular-workspace/projects/trimble-oss/modus-angular-components/src/lib/stencil-generated/components.ts
+++ angular-workspace/projects/trimble-oss/modus-angular-components/src/lib/stencil-generated/components.ts
@@ -176,14 +176,14 @@ export declare interface ModusBadge extends Components.ModusBadge {}
 
 
 @ProxyCmp({
-  inputs: ['ariaLabel', 'crumbs']
+  inputs: ['ariaLabel', 'crumbs', 'underline']
 })
 @Component({
   selector: 'modus-breadcrumb',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: '<ng-content></ng-content>',
   // eslint-disable-next-line @angular-eslint/no-inputs-metadata-property
-  inputs: ['ariaLabel', 'crumbs'],
+  inputs: ['ariaLabel', 'crumbs', 'underline'],
 })
 export class ModusBreadcrumb {
   protected el: HTMLElement;
--- stencil-workspace/storybook/stories/components/modus-breadcrumb/modus-breadcrumb.stories.tsx
+++ stencil-workspace/storybook/stories/components/modus-breadcrumb/modus-breadcrumb.stories.tsx
@@ -29,26 +29,18 @@ export default {
   },
 };
 
+const defaultCrumbs = [
+  { id: '1', display: 'Crumb 1' },
+  { id: '2', display: 'Crumb 2' },
+  { id: '3', display: 'Crumb 3' },
+  { id: '4', display: 'Crumb 4' },
+];
+
 const Template = ({underline}) => html`
-  <modus-breadcrumb underline=${underline}></modus-breadcrumb>
-  ${setBreadcrumb()}
+  <modus-breadcrumb underline=${underline} .crumbs=${defaultCrumbs}></modus-breadcrumb>
 `;
 export const Default = Template.bind({});
 Default.args = { underline: false };
+
 export const Underline = Template.bind({});
 Underline.args = { underline: true };
-// The <script> tag cannot be used in the MDX file, so we use this method to
-// set the breadcrumbs for the default story.
-const setBreadcrumb = () => {
-  const tag = document.createElement('script');
-  tag.innerHTML = `
-   document.querySelector('modus-breadcrumb').crumbs = [
-      { id: '1', display: 'Crumb 1' },
-      { id: '2', display: 'Crumb 2' },
-      { id: '3', display: 'Crumb 3' },
-      { id: '4', display: 'Crumb 4' },
-    ];
-  `;
-
-  return tag;
-};
