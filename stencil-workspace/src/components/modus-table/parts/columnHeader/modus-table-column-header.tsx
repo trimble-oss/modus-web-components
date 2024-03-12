21b636c1cf99a63de8d16877ffcca2b1bc5e146a
RoliverJavier <roliverjavier@gmail.com>
2024-03-12 04:58:02 -0400

    changing events to handle the display the group of tooltips

--- stencil-workspace/src/components/modus-table/parts/columnHeader/modus-table-column-header.tsx
+++ stencil-workspace/src/components/modus-table/parts/columnHeader/modus-table-column-header.tsx
@@ -2,12 +2,17 @@ import {
   FunctionalComponent,
   h, // eslint-disable-line @typescript-eslint/no-unused-vars
 } from '@stencil/core';
-import { Header } from '@tanstack/table-core';
-import { KEYBOARD_ENTER } from '../../modus-table.constants';
+import { Column, Header } from '@tanstack/table-core';
+import {
+  KEYBOARD_ENTER,
+  SORT_ASCENDING,
+  SORT_DESCENDING,
+  SORTED_ASCENDING,
+  SORTED_DESCENDING,
+} from '../../modus-table.constants';
 import { ModusTableColumnResizingHandler } from './modus-table-column-resizing-handler';
 import { ModusTableColumnSortIcon } from './modus-table-column-sort-icon';
 import { TableContext } from '../../models/table-context.models';
-
 interface ModusTableColumnHeaderProps {
   context: TableContext;
   header: Header<unknown, unknown>;
@@ -26,6 +31,7 @@ interface ModusTableColumnHeaderProps {
 /**
  * Modus Table Header
  */
+
 export const ModusTableColumnHeader: FunctionalComponent<ModusTableColumnHeaderProps> = ({
   id,
   context,
@@ -35,7 +41,9 @@ export const ModusTableColumnHeader: FunctionalComponent<ModusTableColumnHeaderP
   onMouseEnterResize,
   onMouseLeaveResize,
 }) => {
-  let elementRef: HTMLTableCellElement;
+  let cellElementRef: HTMLTableCellElement;
+  let headerContentRef: HTMLTableCellElement;
+
   const {
     tableInstance: table,
     isColumnResizing,
@@ -45,10 +53,122 @@ export const ModusTableColumnHeader: FunctionalComponent<ModusTableColumnHeaderP
     sortIconStyle,
   } = context;
   const { column, id: headerId, colSpan, isPlaceholder, getSize } = header;
+  /**
+   * To show sorting status.
+   * @param column Data related to the perticular column.
+   * @returns Active sort or sort that will occur.
+   */
+  const showSortingStatus = (column: Column<unknown, unknown>, isColumnResizing: boolean): string => {
+    return isColumnResizing
+      ? '' // When column resize is enabled, we don't show the tooltip.
+      : column.getIsSorted() === 'asc'
+        ? SORTED_ASCENDING
+        : column.getIsSorted() === 'desc'
+          ? SORTED_DESCENDING
+          : column.getNextSortingOrder() === 'asc'
+            ? SORT_ASCENDING
+            : SORT_DESCENDING;
+  };
+
+  const handleSortIconHover = (event: KeyboardEvent) => {
+    if (event.key.toLowerCase() === KEYBOARD_ENTER) {
+      column.toggleSorting();
+      event.preventDefault();
+    }
+    event.stopPropagation();
+  };
+
+  const sorting_status_text = showSortingStatus(column, isColumnResizing);
+
+  const renderContent = () => (
+    <div
+    tabindex={`${!isColumnResizing && columnReorder ? '0' : ''}`}
+    style={{ outline: 'none', zIndex: '500' }}
+    onFocus={() => {
+      cellElementRef.classList.add('header-base');
+    }}
+    onBlur={() => {
+      cellElementRef.classList.remove('header-base');
+    }}>
+    {isPlaceholder ? null : ( // header.isPlaceholder is Required for nested column headers to display empty cell
+      <span
+        class={column.getCanSort() && 'can-sort'}
+        ref={(element: HTMLTableCellElement) => {
+          headerContentRef = element;
+        }}>
+        <modus-tooltip text={sorting_status_text} disabled={!column.getCanSort()}>
+          <span
+            tabindex={`${column.getCanSort() ? '0' : ''}`}
+            onFocus={() => {
+              cellElementRef.firstElementChild.parentNode
+                .querySelector('modus-tooltip')
+                .setAttribute('disabled', 'true');
+            }}
+            onMouseOver={() => {
+              headerContentRef.children[1].setAttribute('disabled', 'true');
+              cellElementRef.firstElementChild.parentNode
+                .querySelector('modus-tooltip')
+                .setAttribute('disabled', 'true');
+            }}
+            onMouseLeave={() => {
+              headerContentRef.children[1].setAttribute('disabled', 'false');
+              cellElementRef.firstElementChild.parentNode
+                .querySelector('modus-tooltip')
+                .setAttribute('disabled', 'false');
+            }}
+            onBlur={() => {
+              cellElementRef.firstElementChild.parentNode
+                .querySelector('modus-tooltip')
+                .setAttribute('disabled', 'false');
+            }}
+            onKeyDown={handleSortIconHover}
+            class={`${column.getCanSort() && column.getIsSorted() ? 'sorted' : ''}`}>
+            {column.columnDef.header}
+          </span>
+        </modus-tooltip>
+        {column.getCanSort() && (
+          <ModusTableColumnSortIcon
+            column={column}
+            sortIconStyle={sortIconStyle}
+            showSortIconOnHover={showSortIconOnHover}
+            sortingStatus={sorting_status_text}
+            onKeyDown={handleSortIconHover}
+            onMouseOver={() => {
+              headerContentRef.children[0].setAttribute('disabled', 'true');
+              cellElementRef.firstElementChild.parentNode
+                .querySelector('modus-tooltip')
+                .setAttribute('disabled', 'true');
+            }}
+            onMouseLeave={() => {
+              headerContentRef.children[1].setAttribute('disabled', 'false');
+              headerContentRef.children[0].setAttribute('disabled', 'false');
+              cellElementRef.firstElementChild.parentNode
+                .querySelector('modus-tooltip')
+                .setAttribute('disabled', 'true');
+            }}
+            onBlur={() => {
+              cellElementRef.firstElementChild.parentNode
+                .querySelector('modus-tooltip')
+                .setAttribute('disabled', 'false');
+            }}
+          />
+        )}
+      </span>
+    )}
+    {/** Column resizing handler */}
+    {column.getCanResize() ? (
+      <ModusTableColumnResizingHandler
+        table={table}
+        header={header}
+        onMouseEnter={() => onMouseEnterResize()}
+        onMouseLeave={() => onMouseLeaveResize()}
+      />
+    ) : null}
+  </div>
+  )
   return (
     <th
       data-accessor-key={headerId}
-      //tabindex={`${!isColumnResizing && columnReorder ? '0' : ''}`}
       key={id}
       colSpan={colSpan}
       /**
@@ -71,47 +191,24 @@ export const ModusTableColumnHeader: FunctionalComponent<ModusTableColumnHeaderP
       role="columnheader"
       scope="col"
       id={id}
-      ref={(element: HTMLTableCellElement) => (elementRef = element)}
-      onMouseDown={(event: MouseEvent) => onDragStart(event, headerId, elementRef, true)}
+      ref={(element: HTMLTableCellElement) => (cellElementRef = element)}
+      onMouseDown={(event: MouseEvent) => onDragStart(event, headerId, cellElementRef, true)}
       onKeyDown={(event: KeyboardEvent) => {
         if (event.key.toLowerCase() === KEYBOARD_ENTER) {
-          onDragStart(event, headerId, elementRef, false);
+          onDragStart(event, headerId, cellElementRef, false);
         }
       }}>
-      <modus-tooltip text="This is a Test" position="bottom" disabled={showSortIconOnHover}>
-        <div
-          tabIndex={0}
-          style={{ outline: 'none' }}
-          onFocus={() => {
-            elementRef.style.border = '1px solid red';
-          }}
-          onBlur={() => {
-            elementRef.style.border = 'none';
-          }}>
-          {isPlaceholder ? null : ( // header.isPlaceholder is Required for nested column headers to display empty cell
-            <div class={column.getCanSort() && 'can-sort'}>
-              <span class={column.getCanSort() && column.getIsSorted() ? 'sorted' : ''}>{column.columnDef.header}</span>
-              {column.getCanSort() && (
-                <ModusTableColumnSortIcon
-                  column={column}
-                  sortIconStyle={sortIconStyle}
-                  showSortIconOnHover={showSortIconOnHover}
-                  isColumnResizing={isColumnResizing}
-                />
-              )}
-            </div>
-          )}
-          {/** Column resizing handler */}
-          {column.getCanResize() ? (
-            <ModusTableColumnResizingHandler
-              table={table}
-              header={header}
-              onMouseEnter={() => onMouseEnterResize()}
-              onMouseLeave={() => onMouseLeaveResize()}
-            />
-          ) : null}
-        </div>
-      </modus-tooltip>
+        {columnReorder && (
+          <modus-tooltip class="table-header-tooltip" text="Reorder column" position="bottom">
+          {renderContent()}
+          </modus-tooltip>
+        )}
+        {
+          !columnReorder && (
+            renderContent()
+          )
+        }
+     
     </th>
   );
 };
--- stencil-workspace/src/components/modus-table/parts/columnHeader/modus-table-column-sort-icon.tsx
+++ stencil-workspace/src/components/modus-table/parts/columnHeader/modus-table-column-sort-icon.tsx
@@ -5,19 +5,19 @@ import {
 } from '@stencil/core';
 import { Column, SortDirection } from '@tanstack/table-core';
 import { ModusIconMap } from '../../../../icons/ModusIconMap';
-import {
-  KEYBOARD_ENTER,
-  SORT_ASCENDING,
-  SORT_DESCENDING,
-  SORTED_ASCENDING,
-  SORTED_DESCENDING,
-} from '../../modus-table.constants';
+import { KEYBOARD_ENTER } from '../../modus-table.constants';
 
 interface ModusTableColumnSortIconProps {
   column: Column<unknown, unknown>;
   sortIconStyle: 'alphabetical' | 'directional';
   showSortIconOnHover: boolean;
-  isColumnResizing: boolean;
+  sortingStatus: string;
+  onKeyDown: (e) => void;
+  onMouseOver?: () => void;
+  onMouseLeave?: () => void;
+  onFocus?: () => void;
+  onBlur?: () => void;
+  showToolTipText?: boolean;
 }
 
 const ICON_SIZE = '16';
@@ -60,23 +60,6 @@ function getAlphabeticalSortIcon(direction: false | SortDirection): string {
 }
 
 /**
- * To show sorting status.
- * @param column Data related to the perticular column.
- * @returns Active sort or sort that will occur.
- */
-function showSortingStatus(column: Column<unknown, unknown>, isColumnResizing: boolean): string {
-  return isColumnResizing
-    ? '' // When column resize is enabled, we don't show the tooltip.
-    : column.getIsSorted() === 'asc'
-      ? SORTED_ASCENDING
-      : column.getIsSorted() === 'desc'
-        ? SORTED_DESCENDING
-        : column.getNextSortingOrder() === 'asc'
-          ? SORT_ASCENDING
-          : SORT_DESCENDING;
-}
-
-/**
  * Toggles column sort on 'Enter' key press.
  * @param column Data related to the perticular column.
  * @param event Keyboard event.
@@ -93,18 +76,29 @@ export const ModusTableColumnSortIcon: FunctionalComponent<ModusTableColumnSortI
   column,
   sortIconStyle,
   showSortIconOnHover,
-  isColumnResizing,
+  // showToolTipText,
+  sortingStatus,
+  onKeyDown,
+  onMouseOver,
+  onFocus,
+  onBlur,
+  onMouseLeave,
 }) => {
   return (
-    <modus-tooltip text={showSortingStatus(column, isColumnResizing)} position="bottom">
+    <modus-tooltip class="modus-tooltip-sort-icon" text={sortingStatus} position="bottom">
       {
         <span
+          style={{ border: '1px solid green', zIndex: '1000' }}
           tabindex="0"
-          aria-label={showSortingStatus(column, isColumnResizing)}
+          aria-label={sortingStatus}
           role="button"
           onClick={column.getToggleSortingHandler()}
-          onKeyDown={(event) => sortOnKeyDown(column, event)}
+          onKeyDown={(event) => (onKeyDown ? onKeyDown(event) : sortOnKeyDown(column, event))}
           onMouseDown={(event: MouseEvent) => event.stopPropagation()}
+          onMouseEnter={onMouseOver}
+          onMouseLeave={onMouseLeave}
+          onFocus={onFocus}
+          onBlur={onBlur}
           class="sort-icon-container">
           <span
             class={`sort-icon
