871c6472412d95103fe11a24822e5c05f41c750a
Elisha Sam Peter Prabhu <Elisha_SamPeterPrabhu@Trimble.com>
2024-03-20 19:39:58 +0530

    Removed extra constants

--- stencil-workspace/src/components/modus-buttongroup/modus-button-group.models.tsx
+++ stencil-workspace/src/components/modus-buttongroup/modus-button-group.models.tsx
@@ -1,19 +1,3 @@
-import {
-  OUTLINE_STYLE,
-  FILL_STYLE,
-  PRIMARY_VARIANT,
-  SECONDARY_VARIANT,
-  SINGLE_SELECT_TYPE,
-  DEFAULT_SELECT__TYPE,
-  LEFT_BUTTON_POSITION,
-  RIGHT_BUTTON_POSITION,
-  CENTER_BUTTON_POSITION,
-} from './modus-button-group.constants';
+import { SINGLE_SELECT_TYPE, DEFAULT_SELECT__TYPE } from './modus-button-group.constants';
 
-export type ButtonGroupStyle = typeof OUTLINE_STYLE | typeof FILL_STYLE;
-export type ButtonGroupVariant = typeof PRIMARY_VARIANT | typeof SECONDARY_VARIANT;
 export type ButtonGroupSelectionType = typeof SINGLE_SELECT_TYPE | typeof DEFAULT_SELECT__TYPE;
-export type ButtonGroupButtonPosition =
-  | typeof LEFT_BUTTON_POSITION
-  | typeof RIGHT_BUTTON_POSITION
-  | typeof CENTER_BUTTON_POSITION;
