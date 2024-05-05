17fcf8a1e6270440f55fc1420fbac915893908fb
Austin O'Neil <austin.m.oneil@gmail.com>
2024-05-04 23:20:41 -0700

    e2e test

--- stencil-workspace/src/components/modus-tabs/modus-tabs.e2e.ts
+++ stencil-workspace/src/components/modus-tabs/modus-tabs.e2e.ts
@@ -103,4 +103,27 @@ describe('modus-tabs', () => {
     await page.waitForChanges();
     expect(tabChange).toHaveReceivedEvent();
   });
+
+  it('emits tabChange when focused on tab, typed "tab", and hit "enter" (i.e. keyboard navigation works)', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<modus-tabs></modus-tabs>');
+    const tabChange = await page.spyOnEvent('tabChange');
+
+    const modusTabs = await page.find('modus-tabs');
+    modusTabs.setProperty('tabs', [
+      { active: true, id: 0, label: 'Tab1' },
+      { id: 1, label: 'Tab2' },
+    ]);
+    await page.waitForChanges();
+    const element = await page.find('modus-tabs >>> button.active + button');
+    element.focus();
+
+    await page.keyboard.press('Tab');
+    await page.waitForChanges();
+    await page.keyboard.press('Enter');
+    await page.waitForChanges();
+
+    expect(tabChange).toHaveReceivedEvent();
+  });
 });
