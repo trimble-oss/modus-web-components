38c179c802936e6f3f7ba1717f83aa625bb11918
Elisha Sam Peter Prabhu <Elisha_SamPeterPrabhu@Trimble.com>
2024-05-10 16:50:38 +0530

    Add accept property

--- stencil-workspace/storybook/stories/components/modus-file-dropzone/modus-file-dropzone.stories.tsx
+++ stencil-workspace/storybook/stories/components/modus-file-dropzone/modus-file-dropzone.stories.tsx
@@ -5,6 +5,13 @@ import docs from './modus-file-dropzone-storybook-docs.mdx';
 export default {
   title: 'Components/File Dropzone',
   argTypes: {
+    accept: {
+      name:'accept',
+      description:"The dropzone's accepted file types",
+      table: {
+        type: { summary: 'string' },
+      },
+    },
     ariaLabel: {
       name: 'aria-label',
       description: "The dropzone's aria-label",
@@ -91,6 +98,7 @@ export default {
 };
 
 export const Default = ({
+  accept,
   ariaLabel,
   description,
   dropzoneHeight,
@@ -103,6 +111,7 @@ export const Default = ({
   multiple,
 }) => html`
   <modus-file-dropzone
+    accept=${accept}
     aria-label=${ariaLabel}
     description=${description}
     dropzone-height=${dropzoneHeight}
@@ -116,6 +125,7 @@ export const Default = ({
   </modus-file-dropzone>
 `;
 Default.args = {
+  accept:'',
   ariaLabel: 'dropzone',
   description: 'File dropzone description',
   dropzoneHeight: '200px',
