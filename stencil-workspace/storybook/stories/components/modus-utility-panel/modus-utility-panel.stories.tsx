3b6097007561e038d849717aacea40c032777dc8
Elisha Sam Peter Prabhu <Elisha_SamPeterPrabhu@Trimble.com>
2024-06-17 13:13:56 +0530

    Update story book

--- stencil-workspace/storybook/stories/components/modus-utility-panel/modus-utility-panel.stories.tsx
+++ stencil-workspace/storybook/stories/components/modus-utility-panel/modus-utility-panel.stories.tsx
@@ -6,26 +6,27 @@ export default {
   title: 'Components/Utility Panel',
   argTypes: {
     ariaLabel: {
-      description: "The aria label for the panel",
+      description: 'The aria label for the panel',
       table: {
         type: { summary: 'string' },
       },
     },
     expanded: {
-      description: "The expanded state of side navigation panel and items",
+      description: 'The expanded state of side navigation panel and items',
       table: {
         type: { summary: 'boolean' },
       },
     },
     pushContent: {
-      description: "Specify if the side navigation should push the content to the right or overlay the content",
+      description: 'Specify if the side navigation should push the content to the right or overlay the content',
       table: {
         type: { summary: 'boolean' },
       },
     },
     targetContent: {
-      name: "target-content",
-      description: "Specify the selector for the page's content for which paddings and margins will be set by side navigation based on the `mode`",
+      name: 'target-content',
+      description:
+        "Specify the selector for the page's content for which paddings and margins will be set by side navigation based on the `mode`",
       table: {
         type: { summary: 'string' },
       },
@@ -46,12 +47,7 @@ export default {
   },
 };
 
-const DefaultTemplate = ({
-  ariaLabel,
-  expanded,
-  targetContent,
-  pushContent,
-}) => {
+const DefaultTemplate = ({ ariaLabel, expanded, targetContent, pushContent }) => {
   return html`
     <div id="defaultTemplate">
       <div
@@ -59,15 +55,27 @@ const DefaultTemplate = ({
         <modus-navbar
           id="navbar"
           show-main-menu
-          show-help>
+          .buttons=${[
+            {
+              id: 'addMenu',
+              icon: 'add',
+              tooltip: {
+                text: 'Add',
+              },
+            },
+          ]}>
         </modus-navbar>
       </div>
 
       <div
         id="container"
         style="display:flex; min-height:500px; overflow:hidden; position: relative; box-shadow: 0 0 2px var(--modus-secondary)!important;">
-
-        <modus-utility-panel aria-label=${ariaLabel} target-content="#panelcontent" expanded=${expanded} push-content=${pushContent} target-content=${targetContent}>
+        <modus-utility-panel
+          aria-label=${ariaLabel}
+          target-content="#panelcontent"
+          expanded=${expanded}
+          push-content=${pushContent}
+          target-content=${targetContent}>
           <span slot="header">Utility Panel Header</span>
           <div slot="body">Content of the utility panel goes here.</div>
           <span slot="footer">Utility Panel Footer</span>
@@ -76,14 +84,10 @@ const DefaultTemplate = ({
         <div id="panelcontent" style="padding: 10px;">
           <div id="overview">
             <p>
-              The side navigation of an application provides context through
-              accessible menu options and positions a consistent component to
-              connect to various pages in the application.
-              The side navigation is a collapsible side content of the site’s
-              pages. It is located alongside the page’s primary content. The
-              component is designed to add side content to a fullscreen
-              application. It is activated through the “hamburger” menu in the
-              Navbar.
+              The side navigation of an application provides context through accessible menu options and positions a
+              consistent component to connect to various pages in the application. The side navigation is a collapsible side
+              content of the site’s pages. It is located alongside the page’s primary content. The component is designed to
+              add side content to a fullscreen application. It is activated through the “hamburger” menu in the Navbar.
             </p>
           </div>
         </div>
@@ -94,7 +98,7 @@ const DefaultTemplate = ({
 
 export const Default = DefaultTemplate.bind({});
 
-Default.args={
+Default.args = {
   ariaLabel: 'Utility Panel',
   expanded: false,
   pushContent: true,
@@ -111,10 +115,10 @@ Default.decorators = [
           panel.setAttribute('expanded', (!isExpanded).toString());
         }
       };
-      document.addEventListener('helpOpen', handleHelpOpen);
+      document.addEventListener('buttonClick', handleHelpOpen);
 
       return () => {
-        document.removeEventListener('helpOpen', handleHelpOpen);
+        document.removeEventListener('buttonClick', handleHelpOpen);
       };
     }, []);
 
