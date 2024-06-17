4a99486b1371c7795054cbb5752a5a90be965ce0
ElishaSamPeterPrabhu <122255626+ElishaSamPeterPrabhu@users.noreply.github.com>
2024-06-17 11:41:14 +0530

    Create Toolbar component (#2540)
    * Update logic and color
    
    * Add floating toolbar
    
    Added the toolbar
    updated story book
    
    * Update button readme
    
    * Updated functional logic and storybook
    
    * Update storybook
    
    * Updated dss
    
    * format code
    
    * update css storybook
    
    * Rename and format code

---
+++ stencil-workspace/src/components/modus-toolbar/modus-toolbar.tsx
@@ -0,0 +1,51 @@
+// eslint-disable-next-line
+import { Component, h, Element, Prop, Host } from '@stencil/core';
+
+@Component({
+  tag: 'modus-toolbar',
+  styleUrl: 'modus-toolbar.scss',
+  shadow: true,
+})
+export class ModusToolbar {
+  /** (optional) The button's aria-label. */
+  @Prop() ariaLabel: string | null;
+
+  /** (optional) Disables the button. */
+  @Prop() disabled: boolean;
+
+  @Element() host: HTMLElement;
+
+  renderButtons() {
+    const children = Array.from(this.host.children);
+    const buttons = children.map((child) => {
+      if (child.tagName === 'MODUS-BUTTON') {
+        const className = 'modus-button';
+        const iconOnly = child.getAttribute('icon-only');
+        const label = child.textContent ? child.textContent.trim() : '';
+
+        return (
+          <modus-button
+            button-style="borderless"
+            color="secondary"
+            class={className}
+            disabled={this.disabled}
+            icon-only={iconOnly}>
+            {label}
+          </modus-button>
+        );
+      } else {
+        return <modus-divider></modus-divider>;
+      }
+    });
+
+    return buttons;
+  }
+
+  render() {
+    return (
+      <Host aria-label={this.ariaLabel || undefined} role="toolbar">
+        {this.renderButtons()}
+      </Host>
+    );
+  }
+}
