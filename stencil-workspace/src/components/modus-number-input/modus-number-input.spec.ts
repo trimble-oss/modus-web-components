075d744a52bf10780552a65dba76860a03cd2f5f
ElishaSamPeterPrabhu <122255626+ElishaSamPeterPrabhu@users.noreply.github.com>
2024-06-26 15:55:03 +0530

    Update modus-number-input.spec.ts

--- stencil-workspace/src/components/modus-number-input/modus-number-input.spec.ts
+++ stencil-workspace/src/components/modus-number-input/modus-number-input.spec.ts
@@ -12,7 +12,7 @@ describe('modus-number-input', () => {
         <mock:shadow-root>
           <div class="modus-number-input">
             <div class="input-container medium" part="input-container">
-              <input aria-valuenow="" class="text-align-left" id="mwc_id_0_number-input" inputmode="decimal" tabindex="0" type="text" value="">
+              <input class="text-align-left" id="mwc_id_0_number-input" inputmode="decimal" tabindex="0" type="text" value="">
             </div>
           </div>
         </mock:shadow-root>
