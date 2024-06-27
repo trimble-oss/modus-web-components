7f2745290346bf735b1c5fbc1f508495161c3a18
prashanthr6383 <168108000+prashanthr6383@users.noreply.github.com>
2024-06-27 17:04:46 +0530

    Autocomplete: Shift down to open menu items (#2644)
    * 2563 - autocomplete shift down to open menu items
    
    * 2563 - close options on escape key

--- stencil-workspace/src/components/modus-autocomplete/modus-autocomplete.tsx
+++ stencil-workspace/src/components/modus-autocomplete/modus-autocomplete.tsx
@@ -128,6 +128,7 @@ export class ModusAutocomplete {
   @State() visibleCustomOptions: Array<any> = [];
   @State() disableFiltering = false;
   @State() focusItemIndex = 0;
+  @State() ShowItemsOnKeyDown = false;
   private listId = generateElementId() + '_list';
 
   componentWillLoad(): void {
@@ -148,6 +149,7 @@ export class ModusAutocomplete {
 
     if (this.el !== event.target || !this.el.contains(event.target as Node)) {
       this.hasFocus = false;
+      this.ShowItemsOnKeyDown = false;
     }
   }
 
@@ -175,7 +177,8 @@ export class ModusAutocomplete {
     this.value?.length > 0;
 
   displayOptions = () => {
-    const showOptions = this.showOptionsOnFocus || this.value?.length > 0 || this.disableCloseOnSelect;
+    const showOptions =
+      this.showOptionsOnFocus || this.value?.length > 0 || this.disableCloseOnSelect || this.ShowItemsOnKeyDown;
     return this.hasFocus && showOptions && !this.disabled;
   };
 
@@ -214,10 +217,13 @@ export class ModusAutocomplete {
       return; // Do nothing if event already handled
     }
     if (event.code.toUpperCase() === 'ARROWDOWN') {
+      this.ShowItemsOnKeyDown = true;
       if (this.displayOptions() && !this.displayNoResults()) {
         this.focusItemIndex = 0;
         this.focusOptionItem();
       }
+    } else if (event.code.toUpperCase() === 'ESCAPE') {
+      this.ShowItemsOnKeyDown = false;
     }
   };
 
