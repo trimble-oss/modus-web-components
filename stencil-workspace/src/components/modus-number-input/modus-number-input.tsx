f0f6da74f11bd60869305948f7e9462f22f82a51
Elisha Sam Peter Prabhu <Elisha_SamPeterPrabhu@Trimble.com>
2024-07-01 11:42:00 +0530

    Revert "Update story book"
    This reverts commit 49de404b456df8a9c073d6fe7beffc3d81248332.

--- stencil-workspace/src/components/modus-number-input/modus-number-input.tsx
+++ stencil-workspace/src/components/modus-number-input/modus-number-input.tsx
@@ -20,7 +20,7 @@ export class ModusNumberInput {
   @Prop() decimalCharacter: '.' | ',' = '.';
 
   /** (optional) The number of decimal places. */
-  @Prop() decimalPlaces: number;
+  @Prop() decimalPlaces = 2;
 
   /** (optional) The digit group separator. */
   @Prop() digitGroupSeparator: ' ' | ',' | '.' = ',';
