02d653b6e03722d26c5ca2c73691d2f17cb9b397
Leonardo Rodrigues <leonardo_rodrigues@trimble.com>
2024-07-10 09:31:50 -0300

    add showPointerOnHover to storybook controls

--- stencil-workspace/storybook/stories/components/modus-table/modus-table.stories.tsx
+++ stencil-workspace/storybook/stories/components/modus-table/modus-table.stories.tsx
@@ -313,6 +313,7 @@ const DefaultArgs = {
   columnReorder: false,
   pagination: false,
   showSortIconOnHover: false,
+  showPointerOnHover: false,
   summaryRow: false,
   fullWidth: false,
   pageSizeList: [7, 10, 20],
@@ -353,6 +354,16 @@ export default {
     hover: {
       name: 'hover',
       description: 'Enables hover on table rows',
+      control: 'boolean',
+      table: {
+        defaultValue: { summary: false },
+        type: { summary: 'boolean' },
+      },
+      type: { required: false },
+    },
+    showPointerOnHover: {
+      name: 'showPointerOnHover',
+      description: 'Shows a pointer cursor on hover',
       control: 'boolean',
       table: {
         defaultValue: { summary: false },
@@ -639,6 +650,7 @@ const Template = ({
   density,
   wrapText,
   customSort,
+  showPointerOnHover,
 }) => html`
   <div style="width: 950px">
     <modus-table
@@ -650,6 +662,7 @@ const Template = ({
       density="${density}"
       pagination="${pagination}"
       show-sort-icon-on-hover="${showSortIconOnHover}"
+      show-pointer-on-hover="${showPointerOnHover}"
       summary-row="${summaryRow}"
       full-width="${fullWidth}"
       toolbar="${toolbar}"
