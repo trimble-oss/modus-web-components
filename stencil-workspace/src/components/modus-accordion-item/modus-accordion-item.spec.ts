1da90804b46702b0d2f3f1692f908af6df3999e3
coliff <christianoliff@pm.me>
2024-08-15 21:12:24 +0900

    fix

--- stencil-workspace/src/components/modus-accordion-item/modus-accordion-item.spec.ts
+++ stencil-workspace/src/components/modus-accordion-item/modus-accordion-item.spec.ts
@@ -42,7 +42,7 @@ describe('modus-accordion-item', () => {
             <div aria-controls="mwc_id_1_accordion-item" aria-expanded="false" class="header standard" role="button" tabindex="0">
               <span class="title"></span>
               <div class="chevron-container">
-                <svg class="icon-expand-more" fill="currentColor" height="24" viewBox="0 0 24 24" width="24">
+                <svg class="icon-expand-more" fill="currentColor" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
                 <path d="M15.88 9.29 12 13.17 8.12 9.29a.996.996 0 1 0-1.41 1.41l4.59 4.59c.39.39 1.02.39 1.41 0l4.59-4.59a.996.996 0 0 0 0-1.41c-.39-.38-1.03-.39-1.42 0"></path>
                 </svg>
               </div>
--- stencil-workspace/src/components/modus-modal/modus-modal.spec.ts
+++ stencil-workspace/src/components/modus-modal/modus-modal.spec.ts
@@ -20,7 +20,7 @@ describe('modus-modal', () => {
                   </svg>
                 </div>
                   <div role="button" tabindex="0" aria-label="Close">
-                    <svg class="icon-close" height="24" width="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg class="icon-close" height="24" width="24" viewBox="0 0 24 24" fill="none">
                       <g clip-path="url(#clip0)">
                         <path d="M19 7.30929L17.6907 6L12.5 11.1907L7.30929 6L6 7.30929L11.1907 12.5L6 17.6907L7.30929 19L12.5 13.8093L17.6907 19L19 17.6907L13.8093 12.5L19 7.30929Z" fill="currentColor"></path>
                       </g>
