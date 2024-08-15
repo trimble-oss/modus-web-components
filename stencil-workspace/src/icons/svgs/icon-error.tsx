fa92093cf37248e9a3f308c1f2166a2856f9f489
coliff <christianoliff@pm.me>
2024-08-15 20:30:57 +0900

    Remove unneeded `xmlns="http://www.w3.org/2000/svg"` from inline SVGs
    Saves a few bytes = tiny performance improvement

--- stencil-workspace/src/icons/svgs/icon-error.tsx
+++ stencil-workspace/src/icons/svgs/icon-error.tsx
@@ -8,8 +8,7 @@ export const IconError: FunctionalComponent<IconProps> = (props: IconProps) => (
     height={props.size ?? 16}
     width={props.size ?? 16}
     viewBox="0 0 24 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg">
+    fill="none">
     <path
       fill-rule="evenodd"
       clip-rule="evenodd"
--- stencil-workspace/src/icons/svgs/icon-visibility.tsx
+++ stencil-workspace/src/icons/svgs/icon-visibility.tsx
@@ -9,8 +9,7 @@ export const IconVisibility: FunctionalComponent<IconProps> = (props: IconProps)
     height={props.size ?? 16}
     fill={props.color ?? '#252a2e'}
     onClick={props.onClick ? () => props.onClick() : null}
-    viewBox="0 0 24 24"
-    xmlns="http://www.w3.org/2000/svg">
+    viewBox="0 0 24 24">
     <path d="M12 5.2A10.796 10.796 0 0 0 2 12a10.753 10.753 0 0 0 20 0 10.796 10.796 0 0 0-10-6.8Zm0 11.3a4.5 4.5 0 1 1 4.5-4.5 4.481 4.481 0 0 1-4.5 4.5Zm0-7.2a2.7 2.7 0 1 0 2.7 2.7A2.689 2.689 0 0 0 12 9.3Z" />
   </svg>
 );
