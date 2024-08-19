6dc261f8901d1b037b1177245e47c1504a43b5dc
Christian Oliff <christianoliff@pm.me>
2024-08-19 16:22:26 +0900

    Remove unneeded `xmlns="http://www.w3.org/2000/svg"` from inline SVGs (#2792)
    * Remove unneeded `xmlns="http://www.w3.org/2000/svg"` from inline SVGs
    
    Saves a few bytes = tiny performance improvement
    
    * fix

--- stencil-workspace/src/icons/svgs/icon-remove.tsx
+++ stencil-workspace/src/icons/svgs/icon-remove.tsx
@@ -9,8 +9,7 @@ export const IconRemove: FunctionalComponent<IconProps> = (props: IconProps) => 
     width={props.size ?? 16}
     onClick={props.onClick ? (event) => props.onClick(event) : null}
     viewBox="0 0 24 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg">
+    fill="none">
     <path
       fill-rule="evenodd"
       clip-rule="evenodd"
