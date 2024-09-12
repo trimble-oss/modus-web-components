8050718c1a083b467815d0ad64098265d1dab2cb
Christian Oliff <christianoliff@pm.me>
2024-09-12 14:54:10 +0900

    Docs: Add Accessibility page (#2844)
    * Add Accessibility page
    
    Fixes: #2704
    
    * Update super-linter.yml

--- stencil-workspace/storybook/.storybook/preview.js
+++ stencil-workspace/storybook/.storybook/preview.js
@@ -4,19 +4,16 @@ import yourTheme from './your-theme';
 import { themes } from '@storybook/theming';
 import addons from '@storybook/addons';
 import { DocsContainer } from '@storybook/addon-docs';
-import {
-  useDarkMode
-} from 'storybook-dark-mode';
-import '../../dist/modus-web-components/modus-web-components.css'
+import { useDarkMode } from 'storybook-dark-mode';
+import '../../dist/modus-web-components/modus-web-components.css';
 
 defineCustomElements();
 
 // get channel to listen to event emitter
 const channel = addons.getChannel();
 
-
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
@@ -28,10 +25,10 @@ export const parameters = {
     current: 'light',
     // Override the default dark theme
     dark: { ...themes.dark, appBg: '#252a2e' },
-    stylePreview: true
+    stylePreview: true,
   },
   backgrounds: {
-    disable: true
+    disable: true,
   },
   docs: {
     theme: yourTheme,
@@ -40,15 +37,14 @@ export const parameters = {
     storySort: {
       order: [
         'Introduction',
-        ['Welcome', 'Getting Started', 'Contributing'],
+        ['Welcome', 'Getting Started', 'Accessibility', 'Contributing'],
         'Components',
         'User Inputs',
-        'Framework Integrations'
-      ]
-    }
+        'Framework Integrations',
+      ],
+    },
   },
   previewTabs: {
     'storybook/docs/panel': { index: -1 },
-  }
-}
-
+  },
+};
