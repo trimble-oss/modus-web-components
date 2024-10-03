903879a91dfbed2b00f2d3be5c64b24071b671f8
prashanthr6383 <168108000+prashanthr6383@users.noreply.github.com>
2024-10-03 16:27:43 +0530

    847 - adding test cases

--- stencil-workspace/src/components/modus-file-dropzone/modus-file-dropzone.e2e.ts
+++ stencil-workspace/src/components/modus-file-dropzone/modus-file-dropzone.e2e.ts
@@ -8,4 +8,13 @@ describe('modus-file-dropzone', () => {
     const element = await page.find('modus-file-dropzone');
     expect(element).toHaveClass('hydrated');
   });
+
+  it('renders with disabled state', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<modus-file-dropzone disabled></modus-file-dropzone>');
+    const element = await page.find('modus-file-dropzone');
+    expect(element).toHaveClass('hydrated');
+    expect(element).toHaveAttribute('disabled');
+  });
 });
