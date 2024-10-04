89a040b251650d33b34638934d7064922d3a3d10
Elisha Sam Peter Prabhu <Elisha_SamPeterPrabhu@Trimble.com>
2024-10-04 15:39:04 +0530

    Add reset button

--- stencil-workspace/src/components/modus-file-dropzone/modus-file-dropzone.spec.ts
+++ stencil-workspace/src/components/modus-file-dropzone/modus-file-dropzone.spec.ts
@@ -86,4 +86,106 @@ describe('modus-file-dropzone', () => {
       </modus-file-dropzone>
     `);
   });
+
+  it('renders with maxFileCount error is triggered', async () => {
+    const page = await newSpecPage({
+      components: [ModusFileDropzone],
+      html: '<modus-file-dropzone file-dragged-over-instructions="test instructions"></modus-file-dropzone>',
+    });
+    const dropzone = page.rootInstance as ModusFileDropzone;
+    dropzone.error = 'maxFileCount';
+    await page.waitForChanges();
+    expect(page.root).toEqualHtml(`
+      <modus-file-dropzone role="button" file-dragged-over-instructions="test instructions">
+        <mock:shadow-root>
+          <div class="modus-file-dropzone">
+            <input multiple="" type="file">
+            <div class="header">
+              <label></label>
+              <span></span>
+            </div>
+            <div class="dropzone error" tabindex="0">
+              <svg class="icon-cancel" fill="currentColor" width="36" height="36" viewBox="0 0 32 32">
+                <path d="M21.3027,10.6973a1.503,1.503,0,0,0-2.1211,0L16,13.8789l-3.1821-3.1816a1.5,1.5,0,0,0-2.1211,2.1211L13.8789,16l-3.1821,3.1816a1.5012,1.5012,0,0,0,0,2.1211,1.5363,1.5363,0,0,0,2.1211,0L16,18.1211l3.1816,3.1816a1.5,1.5,0,1,0,2.1211-2.1211L18.1211,16l3.1816-3.1816A1.5012,1.5012,0,0,0,21.3027,10.6973Z"></path>
+                <path d="M16,2A14,14,0,1,0,30,16,14.0158,14.0158,0,0,0,16,2Zm0,26A12,12,0,1,1,28,16,12.0134,12.0134,0,0,1,16,28Z"></path>
+              </svg>
+             <div class="error-messages" role="alert">
+               <modus-button button-style="outline" color="secondary">
+                 Reset
+               </modus-button>
+             </div>
+            </div>
+          </div>
+        </mock:shadow-root>
+      </modus-file-dropzone>
+    `);
+  });
+
+  it('renders with maxTotalFileSize error is triggered', async () => {
+    const page = await newSpecPage({
+      components: [ModusFileDropzone],
+      html: '<modus-file-dropzone file-dragged-over-instructions="test instructions"></modus-file-dropzone>',
+    });
+    const dropzone = page.rootInstance as ModusFileDropzone;
+    dropzone.error = 'maxTotalFileSize';
+    await page.waitForChanges();
+    expect(page.root).toEqualHtml(`
+      <modus-file-dropzone role="button" file-dragged-over-instructions="test instructions">
+        <mock:shadow-root>
+          <div class="modus-file-dropzone">
+            <input multiple="" type="file">
+            <div class="header">
+              <label></label>
+              <span></span>
+            </div>
+            <div class="dropzone error" tabindex="0">
+              <svg class="icon-cancel" fill="currentColor" width="36" height="36" viewBox="0 0 32 32">
+                <path d="M21.3027,10.6973a1.503,1.503,0,0,0-2.1211,0L16,13.8789l-3.1821-3.1816a1.5,1.5,0,0,0-2.1211,2.1211L13.8789,16l-3.1821,3.1816a1.5012,1.5012,0,0,0,0,2.1211,1.5363,1.5363,0,0,0,2.1211,0L16,18.1211l3.1816,3.1816a1.5,1.5,0,1,0,2.1211-2.1211L18.1211,16l3.1816-3.1816A1.5012,1.5012,0,0,0,21.3027,10.6973Z"></path>
+                <path d="M16,2A14,14,0,1,0,30,16,14.0158,14.0158,0,0,0,16,2Zm0,26A12,12,0,1,1,28,16,12.0134,12.0134,0,0,1,16,28Z"></path>
+              </svg>
+             <div class="error-messages" role="alert">
+               <modus-button button-style="outline" color="secondary">
+                 Reset
+               </modus-button>
+             </div>
+            </div>
+          </div>
+        </mock:shadow-root>
+      </modus-file-dropzone>
+    `);
+  });
+
+  it('renders with maxFileNameLength error is triggered', async () => {
+    const page = await newSpecPage({
+      components: [ModusFileDropzone],
+      html: '<modus-file-dropzone file-dragged-over-instructions="test instructions"></modus-file-dropzone>',
+    });
+    const dropzone = page.rootInstance as ModusFileDropzone;
+    dropzone.error = 'maxFileNameLength';
+    await page.waitForChanges();
+    expect(page.root).toEqualHtml(`
+      <modus-file-dropzone role="button" file-dragged-over-instructions="test instructions">
+        <mock:shadow-root>
+          <div class="modus-file-dropzone">
+            <input multiple="" type="file">
+            <div class="header">
+              <label></label>
+              <span></span>
+            </div>
+            <div class="dropzone error" tabindex="0">
+              <svg class="icon-cancel" fill="currentColor" width="36" height="36" viewBox="0 0 32 32">
+                <path d="M21.3027,10.6973a1.503,1.503,0,0,0-2.1211,0L16,13.8789l-3.1821-3.1816a1.5,1.5,0,0,0-2.1211,2.1211L13.8789,16l-3.1821,3.1816a1.5012,1.5012,0,0,0,0,2.1211,1.5363,1.5363,0,0,0,2.1211,0L16,18.1211l3.1816,3.1816a1.5,1.5,0,1,0,2.1211-2.1211L18.1211,16l3.1816-3.1816A1.5012,1.5012,0,0,0,21.3027,10.6973Z"></path>
+                <path d="M16,2A14,14,0,1,0,30,16,14.0158,14.0158,0,0,0,16,2Zm0,26A12,12,0,1,1,28,16,12.0134,12.0134,0,0,1,16,28Z"></path>
+              </svg>
+             <div class="error-messages" role="alert">
+               <modus-button button-style="outline" color="secondary">
+                 Reset
+               </modus-button>
+             </div>
+            </div>
+          </div>
+        </mock:shadow-root>
+      </modus-file-dropzone>
+    `);
+  });
 });
