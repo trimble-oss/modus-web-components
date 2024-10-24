9f900c01b8bacb9087505b744c4b96592e33b2f1
Raymond Boswel <raymondboswel@gmail.com>
2024-10-25 07:17:18 +1300

    Re-run build to update generated files

--- stencil-workspace/src/components.d.ts
+++ stencil-workspace/src/components.d.ts
@@ -558,7 +558,7 @@ export namespace Components {
     }
     interface ModusDatePicker {
         /**
-          * (optional) Function to check if a date is enabled If true, the day will be enabled/interactive. If false, the day will be disabled/non-interactive. The function accepts an ISO 8601 date string of a given day. By default, all days are enabled. Developers can use this function to write custom logic to disable certain days. The function is called for each rendered calendar day, for the previous, current and next month. Custom implementations should be optimized for performance to avoid jank.
+          * (optional) Function to check if a date is enabled If true, the day will be enabled/interactive. If false, the day will be disabled/non-interactive. The function accepts an ISO 8601 date string of a given day. By default, all days are enabled. Developers can use this function to write custom logic to disable certain days. The function is called for each rendered calendar day. This function should be optimized for performance to avoid jank.
          */
         "isDateEnabled": (dateIsoString: string) => boolean | undefined;
         /**
@@ -3542,7 +3542,7 @@ declare namespace LocalJSX {
     }
     interface ModusDatePicker {
         /**
-          * (optional) Function to check if a date is enabled If true, the day will be enabled/interactive. If false, the day will be disabled/non-interactive. The function accepts an ISO 8601 date string of a given day. By default, all days are enabled. Developers can use this function to write custom logic to disable certain days. The function is called for each rendered calendar day, for the previous, current and next month. Custom implementations should be optimized for performance to avoid jank.
+          * (optional) Function to check if a date is enabled If true, the day will be enabled/interactive. If false, the day will be disabled/non-interactive. The function accepts an ISO 8601 date string of a given day. By default, all days are enabled. Developers can use this function to write custom logic to disable certain days. The function is called for each rendered calendar day. This function should be optimized for performance to avoid jank.
          */
         "isDateEnabled"?: (dateIsoString: string) => boolean | undefined;
         /**
