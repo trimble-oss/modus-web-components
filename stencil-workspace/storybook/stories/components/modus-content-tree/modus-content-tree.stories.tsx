89b0f9c3440e39c8d374c291fcd811778459d6e2
ElishaSamPeterPrabhu <122255626+ElishaSamPeterPrabhu@users.noreply.github.com>
2024-11-19 17:31:42 +0530

    Content Tree: Add dark theme colors to action bar (#2982)
    * Add dark theme colors to action bar
    
    * modify css

--- stencil-workspace/storybook/stories/components/modus-content-tree/modus-content-tree.stories.tsx
+++ stencil-workspace/storybook/stories/components/modus-content-tree/modus-content-tree.stories.tsx
@@ -192,6 +192,7 @@ const ActionBarTemplate = ({
   isLastChild,
 }) => html`
 <style>
+
   .toolbar {
     gap: 4px;
     display: flex;
@@ -220,7 +221,7 @@ const ActionBarTemplate = ({
       <modus-button button-style="borderless" aria-label="Add" title="Add" size="small" color="primary" disabled id="add">
         <svg width="24" height="24" viewBox="0 0 24 24">
           <path d="M0,0H24V24H0Z" fill="none" />
-          <path d="M19,13H13v6H11V13H5V11h6V5h2v6h6Z" fill="#252a2e" />
+          <path d="M19,13H13v6H11V13H5V11h6V5h2v6h6Z" fill="var(--icon-fill)" />
         </svg>
       </modus-button>
       <modus-button
@@ -233,7 +234,7 @@ const ActionBarTemplate = ({
         id="remove">
         <svg width="24" height="24" viewBox="0 0 24 24">
           <path d="M0,0H24V24H0Z" fill="none" />
-          <path d="M6,19a2.006,2.006,0,0,0,2,2h8a2.006,2.006,0,0,0,2-2V7H6ZM19,4H15.5l-1-1h-5l-1,1H5V6H19Z" fill="#252a2e" />
+          <path d="M6,19a2.006,2.006,0,0,0,2,2h8a2.006,2.006,0,0,0,2-2V7H6ZM19,4H15.5l-1-1h-5l-1,1H5V6H19Z" fill="var(--icon-fill)" />
         </svg>
       </modus-button>
       <modus-button button-style="borderless" size="small" aria-label="Edit" title="Edit" color="primary" disabled id="edit">
@@ -241,7 +242,7 @@ const ActionBarTemplate = ({
           <path d="M0,0H24V24H0Z" fill="none" />
           <path
             d="M3,17.25V21H6.75L17.81,9.94,14.06,6.19ZM20.71,7.04a1,1,0,0,0,0-1.41L18.37,3.29a1,1,0,0,0-1.41,0L15.13,5.12l3.75,3.75,1.83-1.83Z"
-            fill="#252a2e" />
+            fill="var(--icon-fill)" />
         </svg>
       </modus-button>
       <modus-button button-style="borderless" size="small" aria-label="Copy" title="Copy" color="primary" disabled id="copy">
@@ -249,14 +250,14 @@ const ActionBarTemplate = ({
           <path d="M0,0H24V24H0Z" fill="none" />
           <path
             d="M16,1H4A2.006,2.006,0,0,0,2,3V17H4V3H16Zm3,4H8A2.006,2.006,0,0,0,6,7V21a2.006,2.006,0,0,0,2,2H19a2.006,2.006,0,0,0,2-2V7A2.006,2.006,0,0,0,19,5Zm0,16H8V7H19Z"
-            fill="#252a2e" />
+            fill="var(--icon-fill)" />
         </svg>
       </modus-button>
       <modus-button button-style="borderless" size="small" aria-label="Drag" title="Drag" color="primary" disabled id="drag">
         <svg width="24" height="24" viewBox="0 0 24 24">
           <path
             d="M11,18a2,2,0,1,1-2-2A2.006,2.006,0,0,1,11,18ZM9,10a2,2,0,1,0,2,2A2.006,2.006,0,0,0,9,10ZM9,4a2,2,0,1,0,2,2A2.006,2.006,0,0,0,9,4Zm6,4a2,2,0,1,0-2-2A2.006,2.006,0,0,0,15,8Zm0,2a2,2,0,1,0,2,2A2.006,2.006,0,0,0,15,10Zm0,6a2,2,0,1,0,2,2A2.006,2.006,0,0,0,15,16Z"
-            fill="#252a2e" />
+            fill="var(--icon-fill)" />
         </svg>
       </modus-button>
       <modus-button
@@ -271,7 +272,7 @@ const ActionBarTemplate = ({
           <path d="M0,0H24V24H0Z" fill="rgba(0,0,0,0)" />
           <path
             d="M12,5.83,15.17,9l1.41-1.41L12,3,7.41,7.59,8.83,9Zm0,12.34L8.83,15,7.42,16.41,12,21l4.59-4.59L15.17,15Z"
-            fill="#252a2e" /></svg
+            fill="var(--icon-fill)" /></svg
       ></modus-button>
       <modus-button
         button-style="borderless"
@@ -286,7 +287,7 @@ const ActionBarTemplate = ({
           <path d="M0,0H24V24H0Z" fill="none" />
           <path
             d="M7.41,18.59,8.83,20,12,16.83,15.17,20l1.41-1.41L12,14ZM16.59,5.41,15.17,4,12,7.17,8.83,4,7.41,5.41,12,10Z"
-            fill="#252a2e" />
+            fill="var(--icon-fill)" />
         </svg>
       </modus-button>
     </div>
