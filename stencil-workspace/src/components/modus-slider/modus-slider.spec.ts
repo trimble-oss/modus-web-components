274ed54857c7ebc3ad7f80948fa5b793b7f4d7f3
Suruthi Shri P <suruthishri_p@trimble.com>
2024-11-25 21:49:22 +0530

    fix-slider-selection

--- stencil-workspace/src/components/modus-slider/modus-slider.spec.ts
+++ stencil-workspace/src/components/modus-slider/modus-slider.spec.ts
@@ -10,8 +10,8 @@ describe('modus-slider', () => {
     expect(root).toEqualHtml(`
       <modus-slider>
         <mock:shadow-root>
-          <div class="modus-slider">
-            <input class="slider" id="mwc_id_0_slider" max="100" min="0" type="range">
+          <div class="modus-slider" style="--value-percent: 50%">
+            <input class="slider" id="mwc_id_0_slider" max="100" min="0" type="range" value="50">
           </div>
         </mock:shadow-root>
       </modus-slider>
