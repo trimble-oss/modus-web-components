6a268b6c36c5d1f9599961d7b9a61123b43eb451
ElishaSamPeterPrabhu <122255626+ElishaSamPeterPrabhu@users.noreply.github.com>
2025-01-21 12:44:56 +0530

    Table: Text and Tooltip for Row Action Buttons (#3102)
    * update icons with tooltip
    
    * update props
    
    * format code
    
    * Update modus-table-body.tsx

--- stencil-workspace/storybook/stories/components/modus-table/modus-table.stories.tsx
+++ stencil-workspace/storybook/stories/components/modus-table/modus-table.stories.tsx
@@ -999,8 +999,8 @@ RowActions.args = {
   rowActions: [
     {
       id: '1',
-      icon: 'add',
       label: 'Add',
+      tooltipText: 'Add',
       index: 0,
     },
 
@@ -1008,6 +1008,7 @@ RowActions.args = {
       id: '2',
       icon: 'calendar',
       label: 'calendar',
+      tooltipText: 'Calendar',
       index: 1,
     },
 
@@ -1015,6 +1016,7 @@ RowActions.args = {
       id: '3',
       icon: 'cancel_circle',
       label: 'Cancel',
+      tooltipText: 'Cancel',
       index: 2,
     },
     {
@@ -1022,12 +1024,14 @@ RowActions.args = {
       index: 3,
       icon: 'add',
       label: 'Add',
+      tooltipText: 'Add',
     },
     {
       id: '5',
       index: 4,
       icon: 'delete',
       label: 'Delete',
+      tooltipText: 'Delete',
     },
   ],
   data: makeData(7),
