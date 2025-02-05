9009a3a33ceb32a8e241da9e18e621b3b4a9134d
prashanthr6383 <168108000+prashanthr6383@users.noreply.github.com>
2025-02-05 13:30:54 +0530

    2147 - optimizing test cases

--- stencil-workspace/src/components/modus-side-navigation/modus-side-navigation.e2e.ts
+++ stencil-workspace/src/components/modus-side-navigation/modus-side-navigation.e2e.ts
@@ -380,18 +380,10 @@ it('should not get selected when other item is clicked', async () => {
   await page.setContent(`
   <modus-side-navigation>
     <modus-side-navigation-item id="Test" label="Test" selected>
-    <svg slot="menu-icon" fill="currentColor" height="24" width="24" viewBox="0 0 32 32">
-        <g>
-          <path d="m27.707 14.293-11-11a1 1 0 0 0-1.414 0l-11 11A1 1 0 0 0 5 16h5v12a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V16h5a1 1 0 0 0 .707-1.707z"/>
-        </g>
-      </svg>
+     <svg slot="menu-icon"></svg>
     </modus-side-navigation-item>
     <modus-side-navigation-item id="Test2" label="Test2"/>
-    <svg slot="menu-icon" fill="currentColor" height="24" width="24" viewBox="0 0 32 32">
-        <g>
-          <path d="m27.707 14.293-11-11a1 1 0 0 0-1.414 0l-11 11A1 1 0 0 0 5 16h5v12a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V16h5a1 1 0 0 0 .707-1.707z"/>
-        </g>
-      </svg>
+     <svg slot="menu-icon"></svg>
       </modus-side-navigation-item>
   </modus-side-navigation>`);
 
