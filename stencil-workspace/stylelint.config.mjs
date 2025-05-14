659c65b6e8ee3df4ad9ec8182aa8952158751558
coliff <christianoliff@pm.me>
2025-05-14 13:20:43 +0900

    Update stylelint.config.mjs

--- stencil-workspace/stylelint.config.mjs
+++ stencil-workspace/stylelint.config.mjs
@@ -7,6 +7,7 @@ export default {
     'color-function-notation': null,
     'declaration-property-value-keyword-no-deprecated': null,
     'declaration-block-no-redundant-longhand-properties': null,
+    'declaration-property-value-keyword-no-deprecated': null,
     'no-descending-specificity': null,
     'order/properties-alphabetical-order': true,
     'property-no-vendor-prefix': null,
