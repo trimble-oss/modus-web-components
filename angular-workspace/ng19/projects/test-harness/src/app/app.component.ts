37708978caa78a9944729d65d37ea52ebadcca7b
coliff <christianoliff@pm.me>
2025-05-21 21:53:38 +0900

    Add Angular 19 support

---
+++ angular-workspace/ng19/projects/test-harness/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { ModusAngularComponentsModule } from '../../../trimble-oss/modus-angular-components/src/lib/modus-angular-components.module';
+
+@Component({
+    selector: 'app-root',
+    imports: [ModusAngularComponentsModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'test-harness';
+
+  onValueChange() {
+    console.log('Value Changed');
+  }
+}
