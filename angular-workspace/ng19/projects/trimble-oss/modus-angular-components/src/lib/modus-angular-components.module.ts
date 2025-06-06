cd2f784712a9c90ca63cc851b0087f00c4a1ae11
Elisha Sam Peter Prabhu <122255626+ElishaSamPeterPrabhu@users.noreply.github.com>
2025-06-06 10:46:41 +0530

    fix angular19 build failure

--- angular-workspace/ng19/projects/trimble-oss/modus-angular-components/src/lib/modus-angular-components.module.ts
+++ angular-workspace/ng19/projects/trimble-oss/modus-angular-components/src/lib/modus-angular-components.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { DIRECTIVES } from './stencil-generated';
 
 @NgModule({
-  declarations: [...DIRECTIVES],
-  imports: [],
+  imports: [...DIRECTIVES],
   exports: [...DIRECTIVES],
 })
 export class ModusAngularComponentsModule { }
