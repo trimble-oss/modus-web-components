6d84fd61b26018e2f182aa3a9a278bb666ed9b73
Allan Taylor <allan_taylor@trimble.com>
2025-09-05 14:39:29 +1200

    Update tests

--- stencil-workspace/src/components/modus-file-dropzone/modus-file-dropzone.spec.ts
+++ stencil-workspace/src/components/modus-file-dropzone/modus-file-dropzone.spec.ts
@@ -16,13 +16,11 @@ describe('modus-file-dropzone', () => {
               <label></label>
               <span></span>
             </div>
-            <div class="dropzone" tabindex="0">
+            <div class="browse dropzone" tabindex="0">
               <svg class="upload-cloud" fill="currentColor" width="36" height="36" viewBox="0 0 24 24">
                 <path d="M19.93 11.12c.04-.29.07-.57.07-.87C20 6.8 17.2 4 13.75 4c-2.53 0-4.7 1.5-5.68 3.66-.42-.1-.86-.16-1.32-.16C3.57 7.5 1 10.07 1 13.25S3.57 19 6.75 19H19c2.21 0 4-1.79 4-4a3.99 3.99 0 0 0-3.07-3.88Zm-4.41 1.31h-1.96V17c0 .27-.22.5-.5.5h-2.11c-.27 0-.5-.23-.5-.5v-4.57H8.48c-.23 0-.35-.28-.18-.44l3.35-3.35c.2-.2.51-.2.71 0l3.35 3.35c.16.16.05.44-.18.44Z"/>
               </svg>
-                <div class="browse">
-                  Drag files here or browse to upload.
-                </div>
+              Drag files here or browse to upload.
             </div>
           </div>
         </mock:shadow-root>
@@ -44,13 +42,11 @@ describe('modus-file-dropzone', () => {
               <label></label>
               <span></span>
             </div>
-            <div class="dropzone" tabindex="0">
+            <div class="browse dropzone" tabindex="0">
               <svg class="upload-cloud" fill="currentColor" width="36" height="36" viewBox="0 0 24 24">
                 <path d="M19.93 11.12c.04-.29.07-.57.07-.87C20 6.8 17.2 4 13.75 4c-2.53 0-4.7 1.5-5.68 3.66-.42-.1-.86-.16-1.32-.16C3.57 7.5 1 10.07 1 13.25S3.57 19 6.75 19H19c2.21 0 4-1.79 4-4a3.99 3.99 0 0 0-3.07-3.88Zm-4.41 1.31h-1.96V17c0 .27-.22.5-.5.5h-2.11c-.27 0-.5-.23-.5-.5v-4.57H8.48c-.23 0-.35-.28-.18-.44l3.35-3.35c.2-.2.51-.2.71 0l3.35 3.35c.16.16.05.44-.18.44Z"/>
               </svg>
-                <div class="browse">
-                  test instructions
-                </div>
+              test instructions
             </div>
           </div>
         </mock:shadow-root>
@@ -233,13 +229,11 @@ describe('modus-file-dropzone', () => {
              <label></label>
              <span></span>
            </div>
-           <div class="dropzone" tabindex="0">
+           <div class="browse dropzone" tabindex="0">
              <svg class="upload-cloud" fill="currentColor" height="36" viewBox="0 0 24 24" width="36">
                <path d="M19.93 11.12c.04-.29.07-.57.07-.87C20 6.8 17.2 4 13.75 4c-2.53 0-4.7 1.5-5.68 3.66-.42-.1-.86-.16-1.32-.16C3.57 7.5 1 10.07 1 13.25S3.57 19 6.75 19H19c2.21 0 4-1.79 4-4a3.99 3.99 0 0 0-3.07-3.88Zm-4.41 1.31h-1.96V17c0 .27-.22.5-.5.5h-2.11c-.27 0-.5-.23-.5-.5v-4.57H8.48c-.23 0-.35-.28-.18-.44l3.35-3.35c.2-.2.51-.2.71 0l3.35 3.35c.16.16.05.44-.18.44Z"></path>
              </svg>
-             <div class="browse">
-               Drag files here or browse to upload.
-             </div>
+             Drag files here or browse to upload.
            </div>
          </div>
        </mock:shadow-root>
